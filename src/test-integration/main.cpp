5f3a3b0211638b9d607848415195527b3101a682
Timothy A. Bish <tabish@apache.org>
2006-12-24 02:35:35 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-33
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/activemq-cpp/trunk/activemq-cpp@489967 13f79535-47bb-0310-9956-ffa450edef68

--- src/test-integration/main.cpp
+++ src/test-integration/main.cpp
@@ -19,18 +19,28 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/TestResult.h>
+#include <iostream>
 
 int main( int argc, char **argv)
 {
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest( registry.makeTest() );
-
-    // Shows a message as each test starts
-    CppUnit::BriefTestProgressListener listener;
-    runner.eventManager().addListener( &listener );
+    try
+    {
+        CppUnit::TextUi::TestRunner runner;
+        CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+        runner.addTest( registry.makeTest() );
     
-    bool wasSuccessful = runner.run( "", false );
-    return !wasSuccessful;
+        // Shows a message as each test starts
+        CppUnit::BriefTestProgressListener listener;
+        runner.eventManager().addListener( &listener );
+        
+        bool wasSuccessful = runner.run( "", false );
+        return !wasSuccessful;
+    }
+    catch(...) {
+        std::cout << "----------------------------------------" << std::endl;
+        std::cout << "- AN ERROR HAS OCCURED:                -" << std::endl;
+        std::cout << "- Do yuo have a Broker Running?        -" << std::endl;
+        std::cout << "----------------------------------------" << std::endl;        
+    }
 }
 
