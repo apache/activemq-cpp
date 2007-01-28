25a01881661ebe7026b6d714203e7164593d3230
Timothy A. Bish <tabish@apache.org>
2007-01-28 21:49:57 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-30
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/activemq-cpp/trunk/activemq-cpp@500896 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/activemq/connector/openwire/commands/ActiveMQTempTopicTest.h
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ACTIVEMQ_CONNECTOR_OPENWIRE_COMMANDS_ACTIVEMQTEMPTOPICTEST_H_
+#define _ACTIVEMQ_CONNECTOR_OPENWIRE_COMMANDS_ACTIVEMQTEMPTOPICTEST_H_
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <activemq/connector/openwire/commands/ActiveMQDestination.h>
+
+namespace activemq{
+namespace connector{
+namespace openwire{
+namespace commands{
+
+    class ActiveMQTempTopicTest : public CppUnit::TestFixture {
+        
+        CPPUNIT_TEST_SUITE( ActiveMQTempTopicTest );
+        CPPUNIT_TEST( test );
+        CPPUNIT_TEST_SUITE_END();
+
+    public:
+    
+        ActiveMQTempTopicTest() {}
+        virtual ~ActiveMQTempTopicTest() {}
+        
+        void test();
+    };
+
+}}}}
+
+#endif /*_ACTIVEMQ_CONNECTOR_OPENWIRE_COMMANDS_ACTIVEMQTEMPTOPICTEST_H_*/
