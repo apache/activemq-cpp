f8c16256ee5b5252e294255fbd771195dc273d30
Nathan Christopher Mittler <nmittler@apache.org>
2007-02-08 03:12:27 +0000

    [AMQCPP-30] Adding first test for BaseDataStreamMarshaller
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@504774 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/test/activemq/connector/openwire/marshal/BaseDataStreamMarshallerTest.h
+++ activemq-cpp/src/test/activemq/connector/openwire/marshal/BaseDataStreamMarshallerTest.h
@@ -21,6 +21,9 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <activemq/connector/openwire/marshal/BaseDataStreamMarshaller.h>
+#include <activemq/connector/openwire/commands/DataStructure.h>
+
 namespace activemq{
 namespace connector{
 namespace openwire{
@@ -29,15 +32,185 @@ namespace marshal{
     class BaseDataStreamMarshallerTest : public CppUnit::TestFixture {
         
         CPPUNIT_TEST_SUITE( BaseDataStreamMarshallerTest );
-        CPPUNIT_TEST( test );
-        CPPUNIT_TEST_SUITE_END();
+        CPPUNIT_TEST( testLooseMarshal );
+        CPPUNIT_TEST_SUITE_END();                 
+        
+    public:
+    
+        class SimpleDataStructure : public commands::BaseDataStructure {
+            
+        public:
+        
+            bool boolValue;
+            char charValue;
+            short shortValue;
+            int intValue;
+            long long longValue;
+            float floatValue;
+            double doubleValue;
+            std::string stringValue;
+                        
+        public:
+        
+            const static unsigned char TYPE = 0xFF;
+            
+            virtual ~SimpleDataStructure(){}
+            
+            virtual unsigned char getDataStructureType() const {
+                return TYPE;
+            }
+            
+            virtual DataStructure* cloneDataStructure() const {
+                SimpleDataStructure* s = new SimpleDataStructure();
+                s->copyDataStructure( this );
+                return s;
+            }
+
+            virtual void copyDataStructure( const DataStructure* src ){
+                const SimpleDataStructure* srcObj = dynamic_cast<const SimpleDataStructure*>(src);
+                if( srcObj == NULL ){
+                    return;
+                }
+                boolValue = srcObj->boolValue;
+                charValue = srcObj->charValue;
+                shortValue = srcObj->shortValue;
+                intValue = srcObj->intValue;
+                longValue = srcObj->longValue;
+                floatValue = srcObj->floatValue;
+                doubleValue = srcObj->doubleValue;
+                stringValue = srcObj->stringValue;
+            }
+        };
+        
+        class SimpleDataStructureMarshaller : public BaseDataStreamMarshaller{
+        public:
+        
+            virtual ~SimpleDataStructureMarshaller(){}
+         
+            virtual commands::DataStructure* createObject() const {
+                return new SimpleDataStructure();
+            }
+            
+            virtual unsigned char getDataStructureType() const {
+                return SimpleDataStructure::TYPE;
+            }
+            
+            virtual void tightUnmarshal( OpenWireFormat* wireFormat,
+                                     commands::DataStructure* dataStructure,
+                                     io::DataInputStream* dataIn,
+                                     utils::BooleanStream* bs ) throw( io::IOException ) 
+             {
+                BaseDataStreamMarshaller::tightUnmarshal( wireFormat, dataStructure, dataIn, bs );
+
+                SimpleDataStructure* info =
+                    dynamic_cast<SimpleDataStructure*>( dataStructure );
+                    
+                info->boolValue = bs->readBoolean();
+                info->charValue = dataIn->readChar();
+                info->shortValue = dataIn->readShort();
+                info->intValue = dataIn->readInt();
+                info->longValue = tightUnmarshalLong( wireFormat, dataIn, bs );
+                info->floatValue = dataIn->readFloat();
+                info->doubleValue = dataIn->readDouble();
+                info->stringValue = tightUnmarshalString( dataIn, bs );
+             }
+
+            virtual int tightMarshal1( OpenWireFormat* wireFormat,
+                                       commands::DataStructure* dataStructure,
+                                       utils::BooleanStream* bs ) throw( io::IOException )
+            {
+                SimpleDataStructure* info =
+                    dynamic_cast<SimpleDataStructure*>( dataStructure );
+            
+                int rc = BaseDataStreamMarshaller::tightMarshal1( wireFormat, dataStructure, bs );
+                bs->writeBoolean( info->boolValue );
+                rc += tightMarshalLong1( wireFormat, info->longValue, bs );
+                rc += tightMarshalString1( info->stringValue, bs );
+            
+                return 19 + rc;
+            }
+    
+            virtual void tightMarshal2( OpenWireFormat* wireFormat,
+                                        commands::DataStructure* dataStructure,
+                                        io::DataOutputStream* dataOut,
+                                        utils::BooleanStream* bs ) throw( io::IOException )
+            {
+                BaseDataStreamMarshaller::tightMarshal2( wireFormat, dataStructure, dataOut, bs );
+
+                SimpleDataStructure* info =
+                    dynamic_cast<SimpleDataStructure*>( dataStructure );
+                    
+                bs->readBoolean();
+                dataOut->writeChar( info->charValue );
+                dataOut->writeShort( info->shortValue );
+                dataOut->writeInt( info->intValue );                
+                tightMarshalLong2(wireFormat, info->longValue, dataOut, bs);
+                dataOut->writeFloat( info->floatValue );
+                dataOut->writeDouble( info->doubleValue );
+                tightMarshalString2( info->stringValue, dataOut, bs );
+                
+            }
+    
+            virtual void looseUnmarshal( OpenWireFormat* wireFormat,
+                                         commands::DataStructure* dataStructure,
+                                         io::DataInputStream* dataIn ) throw( io::IOException )
+            {
+                BaseDataStreamMarshaller::looseUnmarshal( wireFormat, dataStructure, dataIn );
+                SimpleDataStructure* info =
+                    dynamic_cast<SimpleDataStructure*>( dataStructure );
+                    
+                info->boolValue = dataIn->readBoolean();
+                info->charValue = dataIn->readChar();
+                info->shortValue = dataIn->readShort();
+                info->intValue = dataIn->readInt();
+                info->longValue = looseUnmarshalLong( wireFormat, dataIn );
+                info->floatValue = dataIn->readFloat();
+                info->doubleValue = dataIn->readDouble();
+                info->stringValue = looseUnmarshalString( dataIn );
+            }
+    
+            virtual void looseMarshal( OpenWireFormat* wireFormat,
+                                       commands::DataStructure* dataStructure,
+                                       io::DataOutputStream* dataOut ) throw( io::IOException )
+            {
+                SimpleDataStructure* info =
+                    dynamic_cast<SimpleDataStructure*>( dataStructure );
+                BaseDataStreamMarshaller::looseMarshal( wireFormat, dataStructure, dataOut );
+    
+                dataOut->writeBoolean( info->boolValue );
+                dataOut->writeChar( info->charValue );
+                dataOut->writeShort( info->shortValue );
+                dataOut->writeInt( info->intValue );
+                looseMarshalLong( wireFormat, info->longValue, dataOut );
+                dataOut->writeFloat( info->floatValue );
+                dataOut->writeDouble( info->doubleValue );
+                looseMarshalString( info->stringValue, dataOut );
+            }
+        };
+        
+    private:
+    
+        SimpleDataStructure dataStructure;
 
     public:
     
         BaseDataStreamMarshallerTest() {}
         virtual ~BaseDataStreamMarshallerTest() {}
+        
+        virtual void setUp(){
+            dataStructure.boolValue = true;
+            dataStructure.charValue = 'a';
+            dataStructure.shortValue = 1000;
+            dataStructure.intValue = 100000;
+            dataStructure.longValue = 1000000;
+            dataStructure.floatValue = 10.3f;
+            dataStructure.doubleValue = 20.1;
+            dataStructure.stringValue = "hello world";
+        } 
+        virtual void tearDown(){
+        }
     
-        void test();
+        void testLooseMarshal();
         
     };
 
