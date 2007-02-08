c8d6f8c7c6915df1087520c7113f83a914b38d26
Timothy A. Bish <tabish@apache.org>
2007-02-08 15:16:10 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-30
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@504926 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/activemq/connector/stomp/StompConnectorFactory.cpp
+++ src/main/activemq/connector/stomp/StompConnectorFactory.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <activemq/connector/stomp/StompConnectorFactory.h>
 #include <activemq/connector/stomp/StompConnector.h>
 #include <activemq/connector/Connector.h>
@@ -25,8 +25,6 @@ using namespace activemq::util;
 using namespace activemq::transport;
 using namespace activemq::connector;
 using namespace activemq::connector::stomp;
-
-////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 Connector* StompConnectorFactory::createConnector(
