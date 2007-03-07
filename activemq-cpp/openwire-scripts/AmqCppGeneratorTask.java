649a0878996788994e4e5696f54c0d2991ea6842
Timothy A. Bish <tabish@apache.org>
2007-03-07 00:37:18 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-30
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@515395 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/openwire-scripts/AmqCppGeneratorTask.java
+++ activemq-cpp/openwire-scripts/AmqCppGeneratorTask.java
@@ -27,110 +27,117 @@ import org.codehaus.jam.JamServiceFactory;
 import org.codehaus.jam.JamServiceParams;
 
 /**
- * 
+ *
  * @version $Revision: 384826 $
  */
 public class AmqCppGeneratorTask extends Task {
-	
-	int version = 2;
-	File source = new File(".");
-	File target = new File(".");
-	
+
+    int version = 2;
+    File source = new File(".");
+    File target = new File(".");
+
     public static void main(String[] args) {
-    	
+
         Project project = new Project();
         project.init();
-    	AmqCppGeneratorTask generator = new AmqCppGeneratorTask();
-    	generator.setProject(project);
-    	
-    	if( args.length > 0 ) {
-    		generator.version = Integer.parseInt(args[0]);
+        AmqCppGeneratorTask generator = new AmqCppGeneratorTask();
+        generator.setProject(project);
+
+        if( args.length > 0 ) {
+            generator.version = Integer.parseInt(args[0]);
             System.out.println( "Generator Version: " + Integer.parseInt(args[0]) );
         }
 
-    	if( args.length > 1 ) {
-    		generator.source = new File(args[1]);
+        if( args.length > 1 ) {
+            generator.source = new File(args[1]);
             System.out.println( "Generator Source: " + generator.source );
-        }  
-    	
-    	if( args.length > 2 ) {
-    		generator.target = new File(args[2]);
+        }
+
+        if( args.length > 2 ) {
+            generator.target = new File(args[2]);
             System.out.println( "Generator Source: " + generator.target );
-        }    	
-    	
-    	generator.execute();
-	}
-    
+        }
+
+        generator.execute();
+    }
+
     public void execute() throws BuildException {
         try {
-        	
-        	String sourceDir = source+"/src/main/java";
-        	
+
+            String sourceDir = source+"/src/main/java";
+
             System.out.println("Parsing source files in: " + sourceDir);
 
             JamServiceFactory jamServiceFactory = JamServiceFactory.getInstance();
-            JamServiceParams params = jamServiceFactory.createServiceParams();            
-            File[] dirs = new File[]{new File(sourceDir)};            
+            JamServiceParams params = jamServiceFactory.createServiceParams();
+            File[] dirs = new File[]{new File(sourceDir)};
             params.includeSourcePattern(dirs, "**/*.java");
             JamService jam = jamServiceFactory.createService(params);
 
             {
-            	AmqCppClassesGenerator script = new AmqCppClassesGenerator();
-	        	script.setJam(jam);
-	        	script.setTargetDir(target+"/src/main");
-	        	script.setOpenwireVersion(version);
-	        	script.run();
+                AmqCppClassesGenerator script = new AmqCppClassesGenerator();
+                script.setJam(jam);
+                script.setTargetDir(target+"/src/main");
+                script.setOpenwireVersion(version);
+                script.run();
             }
             {
                 AmqCppHeadersGenerator script = new AmqCppHeadersGenerator();
-	        	script.setJam(jam);
-	        	script.setTargetDir(target+"/src/main");
-	        	script.setOpenwireVersion(version);
-	        	script.run();
+                script.setJam(jam);
+                script.setTargetDir(target+"/src/main");
+                script.setOpenwireVersion(version);
+                script.run();
             }
             {
                 AmqCppMarshallingHeadersGenerator script = new AmqCppMarshallingHeadersGenerator();
-	        	script.setJam(jam);
-	        	script.setTargetDir(target+"/src/main");
-	        	script.setOpenwireVersion(version);
-	        	script.run();
+                script.setJam(jam);
+                script.setTargetDir(target+"/src/main");
+                script.setOpenwireVersion(version);
+                script.run();
+            }
+            {
+                AmqCppTestMarshallingHeadersGenerator script = new AmqCppTestMarshallingHeadersGenerator();
+                script.setJam(jam);
+                script.setTargetDir(target+"/src/test");
+                script.setOpenwireVersion(version);
+                script.run();
             }
             {
                 AmqCppMarshallingClassesGenerator script = new AmqCppMarshallingClassesGenerator();
-	        	script.setJam(jam);
-	        	script.setTargetDir(target+"/src/main");
-	        	script.setOpenwireVersion(version);
-	        	script.run();
+                script.setJam(jam);
+                script.setTargetDir(target+"/src/main");
+                script.setOpenwireVersion(version);
+                script.run();
             }
-            
-            
+
+
         } catch (Exception e) {
-        	throw new BuildException(e);
+            throw new BuildException(e);
         }
     }
 
-	public int getVersion() {
-		return version;
-	}
+    public int getVersion() {
+        return version;
+    }
 
-	public void setVersion(int version) {
-		this.version = version;
-	}
+    public void setVersion(int version) {
+        this.version = version;
+    }
 
-	public File getSource() {
-		return source;
-	}
+    public File getSource() {
+        return source;
+    }
 
-	public void setSource(File basedir) {
-		this.source = basedir;
-	}
+    public void setSource(File basedir) {
+        this.source = basedir;
+    }
 
-	public File getTarget() {
-		return target;
-	}
+    public File getTarget() {
+        return target;
+    }
 
-	public void setTarget(File target) {
-		this.target = target;
-	}
+    public void setTarget(File target) {
+        this.target = target;
+    }
 
 }
