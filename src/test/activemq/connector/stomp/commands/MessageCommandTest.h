9539ebdbd35b0aa1c650dbd1be070c5edb61b181
Timothy A. Bish <tabish@apache.org>
2007-04-05 21:39:43 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-96
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@525971 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/activemq/connector/stomp/commands/MessageCommandTest.h
+++ src/test/activemq/connector/stomp/commands/MessageCommandTest.h
@@ -43,39 +43,39 @@ namespace commands{
         CPPUNIT_TEST_SUITE_END();
 
     protected:
-    
+
         class TestAckHandler : public core::ActiveMQAckHandler
         {
         public:
-        
+
             TestAckHandler(void) { wasAcked = false; }
             virtual ~TestAckHandler(void) {}
-            
+
             virtual void acknowledgeMessage( const core::ActiveMQMessage* message AMQCPP_UNUSED)
-                throw ( cms::CMSException ) 
+                throw ( cms::CMSException )
             {
                 wasAcked = true;
             }
-            
+
         public:
-        
+
             bool wasAcked;
 
         };
-    
+
     public:
 
-    	MessageCommandTest() {}
-    	virtual ~MessageCommandTest() {}
+        MessageCommandTest() {}
+        virtual ~MessageCommandTest() {}
 
         void test(void)
         {
             TestAckHandler ackHandler;
             MessageCommand cmd;
 
-            CPPUNIT_ASSERT( cmd.getStompCommandId() == 
+            CPPUNIT_ASSERT( cmd.getStompCommandId() ==
                             CommandConstants::SEND );
-            
+
             CPPUNIT_ASSERT( cmd.isResponseRequired() == false );
             cmd.setResponseRequired( true );
             cmd.setCommandId( 123 );
@@ -85,53 +85,53 @@ namespace commands{
             CPPUNIT_ASSERT( cmd.getCorrelationId() == 0 );
             CPPUNIT_ASSERT( cmd.getTransactionId() == "" );
             cmd.setTransactionId( "ID:123456" );
-            CPPUNIT_ASSERT( std::string( cmd.getTransactionId() ) == 
+            CPPUNIT_ASSERT( std::string( cmd.getTransactionId() ) ==
                             "ID:123456" );
             StompTopic topic("testTopic");
             cmd.setCMSDestination( &topic );
-            
+
             StompFrame* frame = cmd.marshal().clone();
-            
+
             CPPUNIT_ASSERT( frame != NULL );
-            
+
             MessageCommand cmd1( frame );
-            
+
             CPPUNIT_ASSERT( cmd.getCommandId() == cmd1.getCommandId() );
             CPPUNIT_ASSERT( cmd.getStompCommandId() == CommandConstants::SEND );
             CPPUNIT_ASSERT( cmd.isResponseRequired() == cmd1.isResponseRequired() );
             CPPUNIT_ASSERT( cmd.getCorrelationId() == cmd1.getCorrelationId() );
             CPPUNIT_ASSERT( std::string(cmd.getTransactionId()) == cmd1.getTransactionId() );
-            
+
             cmd.setAckHandler( &ackHandler );
             cmd.acknowledge();
             CPPUNIT_ASSERT( ackHandler.wasAcked == true );
-            
-            CPPUNIT_ASSERT( 
+
+            CPPUNIT_ASSERT(
                 cmd.getProperties().hasProperty( "test" ) == false );
             cmd.getProperties().setProperty( "test", "value" );
-            CPPUNIT_ASSERT( 
+            CPPUNIT_ASSERT(
                 cmd.getProperties().hasProperty( "test" ) == true );
-            CPPUNIT_ASSERT( 
-                std::string( cmd.getProperties().getProperty( "test" ) ) == 
+            CPPUNIT_ASSERT(
+                std::string( cmd.getProperties().getProperty( "test" ) ) ==
                 "value" );
-                
+
             CPPUNIT_ASSERT( cmd.getCMSCorrelationID() == "" );
             cmd.setCMSCorrelationID( "ID:1234567" );
-            CPPUNIT_ASSERT( std::string( cmd.getCMSCorrelationID() ) == 
+            CPPUNIT_ASSERT( std::string( cmd.getCMSCorrelationID() ) ==
                             "ID:1234567" );
-            CPPUNIT_ASSERT( cmd.getCMSDeliveryMode() == 
+            CPPUNIT_ASSERT( cmd.getCMSDeliveryMode() ==
                             cms::DeliveryMode::PERSISTENT );
             cmd.setCMSDeliveryMode( cms::DeliveryMode::NON_PERSISTENT );
-            CPPUNIT_ASSERT( cmd.getCMSDeliveryMode() == 
+            CPPUNIT_ASSERT( cmd.getCMSDeliveryMode() ==
                             cms::DeliveryMode::NON_PERSISTENT );
-            CPPUNIT_ASSERT( cmd.getCMSDestination()->toProviderString() == 
+            CPPUNIT_ASSERT( cmd.getCMSDestination()->toProviderString() ==
                             "/topic/testTopic" );
             CPPUNIT_ASSERT( cmd.getCMSExpiration() == 0 );
             cmd.setCMSExpiration( 123 );
             CPPUNIT_ASSERT( cmd.getCMSExpiration() == 123 );
             CPPUNIT_ASSERT( cmd.getCMSMessageID() == "" );
             cmd.setCMSMessageID( "ID:1234567" );
-            CPPUNIT_ASSERT( std::string( cmd.getCMSMessageID() ) == 
+            CPPUNIT_ASSERT( std::string( cmd.getCMSMessageID() ) ==
                             "ID:1234567" );
             CPPUNIT_ASSERT( cmd.getCMSPriority() == 0 );
             cmd.setCMSPriority( 5 );
@@ -141,21 +141,21 @@ namespace commands{
             CPPUNIT_ASSERT( cmd.getCMSRedelivered() == true );
             CPPUNIT_ASSERT( cmd.getCMSReplyTo() == NULL );
             cmd.setCMSReplyTo( &topic );
-            CPPUNIT_ASSERT( cmd.getCMSReplyTo()->toProviderString() == 
+            CPPUNIT_ASSERT( cmd.getCMSReplyTo()->toProviderString() ==
                             "/topic/testTopic" );
             CPPUNIT_ASSERT( cmd.getCMSTimestamp() == 0 );
             cmd.setCMSTimestamp( 123 );
             CPPUNIT_ASSERT( cmd.getCMSTimestamp() == 123 );
             CPPUNIT_ASSERT( cmd.getCMSType() == "" );
             cmd.setCMSType( "test" );
-            CPPUNIT_ASSERT( std::string( cmd.getCMSType() ) == 
+            CPPUNIT_ASSERT( std::string( cmd.getCMSType() ) ==
                             "test" );
             CPPUNIT_ASSERT( cmd.getRedeliveryCount() == 0 );
             cmd.setRedeliveryCount( 123 );
             CPPUNIT_ASSERT( cmd.getRedeliveryCount() == 123 );
 
             cms::Message* cmd2 = cmd.clone();
-            
+
             CPPUNIT_ASSERT( cmd.getCMSPriority() == cmd2->getCMSPriority() );
             CPPUNIT_ASSERT( cmd.getCMSTimestamp() == cmd2->getCMSTimestamp() );
             CPPUNIT_ASSERT( cmd.getCMSExpiration() == cmd2->getCMSExpiration() );
@@ -165,14 +165,14 @@ namespace commands{
             CPPUNIT_ASSERT( std::string(cmd.getCMSType()) == cmd2->getCMSType() );
             CPPUNIT_ASSERT( std::string(cmd.getCMSMessageID()) == cmd2->getCMSMessageID() );
 
-            core::ActiveMQMessage* message = 
+            core::ActiveMQMessage* message =
                 dynamic_cast< core::ActiveMQMessage* >( cmd2 );
-                
+
             CPPUNIT_ASSERT( message != NULL );
-            CPPUNIT_ASSERT( cmd.getRedeliveryCount() == 
+            CPPUNIT_ASSERT( cmd.getRedeliveryCount() ==
                             message->getRedeliveryCount() );
-            
-            StompCommand* cmd4 = 
+
+            StompCommand* cmd4 =
                 dynamic_cast< StompCommand* >( cmd2 );
 
             CPPUNIT_ASSERT( cmd4 != NULL );
@@ -180,7 +180,7 @@ namespace commands{
             CPPUNIT_ASSERT( cmd.getStompCommandId() == cmd4->getStompCommandId() );
             CPPUNIT_ASSERT( cmd.isResponseRequired() == cmd4->isResponseRequired() );
             CPPUNIT_ASSERT( cmd.getCorrelationId() == cmd4->getCorrelationId() );
-            CPPUNIT_ASSERT( std::string(cmd.getTransactionId()) == 
+            CPPUNIT_ASSERT( std::string(cmd.getTransactionId()) ==
                             cmd4->getTransactionId() );
 
             delete cmd2;
@@ -188,43 +188,46 @@ namespace commands{
 
         void testSetProperties(){
             MessageCommand cmd;
-            
+
             cmd.setBooleanProperty( "bool", true );
             CPPUNIT_ASSERT( cmd.getBooleanProperty( "bool" ) == true );
-            
+
             cmd.setByteProperty( "byte", 1 );
             CPPUNIT_ASSERT( cmd.getByteProperty( "byte" ) == 1 );
-            
+
             cmd.setDoubleProperty( "double", 2.2 );
             CPPUNIT_ASSERT( cmd.getDoubleProperty( "double" ) == 2.2 );
-            
+
             cmd.setFloatProperty( "float", 3.3f );
             CPPUNIT_ASSERT( cmd.getFloatProperty( "float" ) == 3.3f );
-            
+
             cmd.setIntProperty( "int", 4 );
             CPPUNIT_ASSERT( cmd.getIntProperty( "int" ) == 4 );
-            
+
             cmd.setLongProperty( "long", 5LL );
             CPPUNIT_ASSERT( cmd.getLongProperty( "long" ) == 5LL );
-            
+
             cmd.setShortProperty( "short", 6 );
             CPPUNIT_ASSERT( cmd.getShortProperty( "short" ) == 6 );
-            
+
             cmd.setStringProperty( "string", "hello" );
-            CPPUNIT_ASSERT( cmd.getStringProperty( "string" ) == "hello" );                        
+            CPPUNIT_ASSERT( cmd.getStringProperty( "string" ) == "hello" );
+
+            cmd.setStringProperty( "JMSXGroupID", "hello" );
+            CPPUNIT_ASSERT( cmd.getStringProperty( "JMSXGroupID" ) == "hello" );
         }
 
         void testFailedGetProperties(){
             MessageCommand cmd;
-            
-            // Test getting a string property that doesn't exist. 
+
+            // Test getting a string property that doesn't exist.
             try {
                 std::string str = cmd.getStringProperty( "text" );
                 CPPUNIT_ASSERT(false);
             } catch( cms::CMSException& e){
             }
-            
-            // Test getting a bool property that doesn't exist. 
+
+            // Test getting a bool property that doesn't exist.
             try {
                 cmd.getBooleanProperty( "text" );
                 CPPUNIT_ASSERT(false);
@@ -235,57 +238,57 @@ namespace commands{
             try{
                 cmd.getIntProperty("string");
                 CPPUNIT_ASSERT(false);
-            } catch( cms::CMSException& e ){                
+            } catch( cms::CMSException& e ){
             }
-            
+
             // Test failed extraction
             try{
                 cmd.setStringProperty("string", "hello");
                 cmd.getIntProperty("string");
                 CPPUNIT_ASSERT(false);
-            } catch( cms::CMSException& e ){                
+            } catch( cms::CMSException& e ){
             }
-        }      
-        
+        }
+
         void testGetPropertyNames(){
-            
+
             MessageCommand cmd;
-            
+
             std::vector<std::string> names = cmd.getPropertyNames();
             CPPUNIT_ASSERT( names.size() == 0 );
-            
+
             cmd.setIntProperty( "int1", 1 );
             names = cmd.getPropertyNames();
             CPPUNIT_ASSERT( names.size() == 1 );
             CPPUNIT_ASSERT( names[0] == "int1" );
-            
+
             cmd.setIntProperty( "int2", 2 );
             names = cmd.getPropertyNames();
             CPPUNIT_ASSERT( names.size() == 2 );
             CPPUNIT_ASSERT( names[0] == "int1" || names[1] == "int1" );
             CPPUNIT_ASSERT( names[0] == "int2" || names[1] == "int2" );
-            
+
             // Make sure headers aren't included in the property names.
             cmd.setCMSExpiration( 10000 );
             names = cmd.getPropertyNames();
             CPPUNIT_ASSERT( names.size() == 2 );
         }
-        
+
         void testClearProperties(){
             MessageCommand cmd;
-            
+
             cmd.setIntProperty( "int1", 1 );
             cmd.setIntProperty( "int2", 2 );
-            
+
             CPPUNIT_ASSERT( cmd.propertyExists( "int1" ) == true );
             CPPUNIT_ASSERT( cmd.propertyExists( "int2" ) == true );
-            
+
             cmd.clearProperties();
-            
+
             CPPUNIT_ASSERT( cmd.propertyExists( "int1" ) == false );
-            CPPUNIT_ASSERT( cmd.propertyExists( "int2" ) == false );                        
+            CPPUNIT_ASSERT( cmd.propertyExists( "int2" ) == false );
         }
-          
+
     };
 
 }}}}
