e7edcf2d01db8208df4de623296627bc559cc6f3
Timothy A. Bish <tabish@apache.org>
2007-04-08 19:34:27 +0000

    Adding additional Documentation
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@526597 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/activemq/transport/Transport.h
+++ src/main/activemq/transport/Transport.h
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #ifndef ACTIVEMQ_TRANSPORT_TRANSPORT_H_
 #define ACTIVEMQ_TRANSPORT_TRANSPORT_H_
 
@@ -29,18 +29,23 @@
 
 namespace activemq{
 namespace transport{
-  
+
     // Forward declarations.
     class CommandListener;
     class CommandReader;
     class CommandWriter;
     class TransportExceptionListener;
-     
+
     /**
      * Interface for a transport layer for command objects.  Callers can
      * send oneway messages or make synchronous requests.  Non-response
      * messages will be delivered to the specified listener object upon
-     * receipt.
+     * receipt.  A user of the Transport can set an exception listener
+     * to be notified of errors that occurs in Threads that the Transport
+     * layer runs.  Since a Transport doesn't know the Wire Format of the
+     * Commands it reads and writes, its up to the managing object to
+     * provide object(s) that implement the CommandReader and CommandWriter
+     * interfaces.
      */
     class Transport
     :
@@ -48,9 +53,9 @@ namespace transport{
         public cms::Closeable
     {
     public:
-     
+
         virtual ~Transport(){}
-        
+
         /**
          * Sends a one-way command.  Does not wait for any response from the
          * broker.
@@ -60,10 +65,10 @@ namespace transport{
          * @throws UnsupportedOperationException if this method is not implemented
          * by this transport.
          */
-        virtual void oneway( Command* command ) 
-            throw( CommandIOException, 
+        virtual void oneway( Command* command )
+            throw( CommandIOException,
                    exceptions::UnsupportedOperationException ) = 0;
-  
+
         /**
          * Sends the given command to the broker and then waits for the response.
          * @param command the command to be sent.
@@ -73,36 +78,36 @@ namespace transport{
          * @throws UnsupportedOperationException if this method is not implemented
          * by this transport.
          */
-        virtual Response* request( Command* command ) 
-            throw( CommandIOException, 
+        virtual Response* request( Command* command )
+            throw( CommandIOException,
                    exceptions::UnsupportedOperationException ) = 0;
-     
+
         /**
          * Assigns the command listener for non-response commands.
          * @param listener the listener.
          */
         virtual void setCommandListener( CommandListener* listener ) = 0;
-     
+
         /**
          * Sets the command reader.
          * @param reader the object that will be used for reading command objects.
          */
         virtual void setCommandReader( CommandReader* reader ) = 0;
-        
+
         /**
          * Sets the command writer.
          * @param writer the object that will be used for writing command objects.
          */
         virtual void setCommandWriter( CommandWriter* writer ) = 0;
-     
+
         /**
          * Sets the observer of asynchronous exceptions from this transport.
          * @param listener the listener of transport exceptions.
          */
-        virtual void setTransportExceptionListener( 
+        virtual void setTransportExceptionListener(
             TransportExceptionListener* listener ) = 0;
     };
-    
+
 }}
 
 #endif /*ACTIVEMQ_TRANSPORT_TRANSPORT_H_*/
