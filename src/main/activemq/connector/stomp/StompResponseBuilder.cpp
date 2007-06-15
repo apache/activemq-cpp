649d9c9eee644893686c6ef41dc5617fb93a1208
Timothy A. Bish <tabish@apache.org>
2007-06-15 19:16:50 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-130
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@547769 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/activemq/connector/stomp/StompResponseBuilder.cpp
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "StompResponseBuilder.h"
+
+#include <activemq/connector/stomp/commands/ConnectCommand.h>
+#include <activemq/connector/stomp/commands/ConnectedCommand.h>
+
+using namespace activemq;
+using namespace activemq::connector;
+using namespace activemq::connector::stomp;
+using namespace activemq::transport;
+
+////////////////////////////////////////////////////////////////////////////////
+Response* StompResponseBuilder::buildResponse( const transport::Command* cmd ){
+
+    // If this command requires a response we don't know what it is
+    // so we throw an exception.
+    if( cmd->isResponseRequired() ) {
+
+        throw transport::CommandIOException( __FILE__, __LINE__,
+            "StompResponseBuilder - unrecognized command" );
+    }
+
+    return NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Command* StompResponseBuilder::buildIncomingCommand( const transport::Command* cmd ){
+
+    const commands::ConnectCommand* connectCommand =
+        dynamic_cast<const commands::ConnectCommand*>(cmd);
+
+    if( connectCommand != NULL ){
+        commands::ConnectedCommand* resp = new commands::ConnectedCommand();
+        resp->setCorrelationId( connectCommand->getCommandId() );
+
+        if( connectCommand->getClientId() == NULL )
+        {
+            resp->setSessionId( sessionId );
+        }
+        else
+        {
+            resp->setSessionId( connectCommand->getClientId() );
+        }
+
+        return resp;
+    }
+
+    return NULL;
+}
