711b5991e00650d04dd8392c2586e3786aab7a1e
Timothy A. Bish <tabish@apache.org>
2007-07-02 15:11:23 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-93
    Submitting optimized? DataOutputStream
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@552510 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/activemq/io/DataOutputStream.cpp
+++ src/main/activemq/io/DataOutputStream.cpp
@@ -56,6 +56,7 @@ void DataOutputStream::write( const std::vector<unsigned char>& buffer )
         }
 
         outputStream->write( &buffer[0], buffer.size() );
+        written += buffer.size();
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
@@ -67,6 +68,7 @@ void DataOutputStream::write( const unsigned char* buffer, std::size_t len )
 
     try {
         outputStream->write( buffer, len );
+        written += len;
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
@@ -80,6 +82,7 @@ void DataOutputStream::write( const unsigned char* buffer,
 
     try {
         outputStream->write( buffer+offset, len );
+        written += len;
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
@@ -91,7 +94,8 @@ void DataOutputStream::writeBoolean( bool value ) throw ( IOException ) {
         unsigned char ivalue = 0;
         value == true ? ivalue = 1 : ivalue = 0;
 
-        this->write( ivalue );
+        outputStream->write( ivalue );
+        written++;
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
@@ -100,32 +104,13 @@ void DataOutputStream::writeBoolean( bool value ) throw ( IOException ) {
 ////////////////////////////////////////////////////////////////////////////////
 void DataOutputStream::writeByte( unsigned char value ) throw ( IOException ) {
     try {
-        this->write( value );
+        outputStream->write( value );
+        written++;
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
 }
 
-////////////////////////////////////////////////////////////////////////////////
-void DataOutputStream::writeShort( short value ) throw ( IOException ) {
-    try {
-        this->writeUnsignedShort( (unsigned short)value );
-    }
-    AMQ_CATCH_RETHROW( IOException )
-    AMQ_CATCHALL_THROW( IOException )
-}
-
-////////////////////////////////////////////////////////////////////////////////
-void DataOutputStream::writeUnsignedShort( unsigned short value )
-    throw ( IOException )
-{
-    try {
-        write( (unsigned char)( (value & 0xFF00) >> 8 ) );
-        write( (unsigned char)( (value & 0x00FF) >> 0 ) );
-    }
-    AMQ_CATCH_RETHROW( IOException )
-    AMQ_CATCHALL_THROW( IOException )
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 void DataOutputStream::writeChar( char value ) throw ( IOException ) {
@@ -137,12 +122,49 @@ void DataOutputStream::writeChar( char value ) throw ( IOException ) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+void DataOutputStream::writeShort( short value ) throw ( IOException ) {
+    try {
+        unsigned char buffer[sizeof(value)];
+
+        buffer[0] = (value & 0xFF00) >> 8;
+        buffer[1] = (value & 0x00FF) >> 0;
+
+        outputStream->write( buffer, sizeof(value) );
+        written += sizeof( value );
+    }
+    AMQ_CATCH_RETHROW( IOException )
+    AMQ_CATCHALL_THROW( IOException )
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void DataOutputStream::writeUnsignedShort( unsigned short value )
+    throw ( IOException )
+{
+    try {
+        unsigned char buffer[sizeof(value)];
+
+        buffer[0] = (value & 0xFF00) >> 8;
+        buffer[1] = (value & 0x00FF) >> 0;
+
+        outputStream->write( buffer, sizeof(value) );
+        written += sizeof( value );
+    }
+    AMQ_CATCH_RETHROW( IOException )
+    AMQ_CATCHALL_THROW( IOException )
+}
+
+////////////////////////////////////////////////////////////////////////////////
 void DataOutputStream::writeInt( int value ) throw ( IOException ) {
     try {
-        this->write( (unsigned char)( (value & 0xFF000000) >> 24 ) );
-        this->write( (unsigned char)( (value & 0x00FF0000) >> 16 ) );
-        this->write( (unsigned char)( (value & 0x0000FF00) >> 8 ) );
-        this->write( (unsigned char)( (value & 0x000000FF) >> 0 ) );
+        unsigned char buffer[sizeof(value)];
+
+        buffer[0] = (value & 0xFF000000) >> 24;
+        buffer[1] = (value & 0x00FF0000) >> 16;
+        buffer[2] = (value & 0x0000FF00) >> 8;
+        buffer[3] = (value & 0x000000FF) >> 0;
+
+        outputStream->write( buffer, sizeof(value) );
+        written += sizeof( value );
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
@@ -151,14 +173,19 @@ void DataOutputStream::writeInt( int value ) throw ( IOException ) {
 ////////////////////////////////////////////////////////////////////////////////
 void DataOutputStream::writeLong( long long value ) throw ( IOException ) {
     try {
-        this->write( (unsigned char)( (value & 0xFF00000000000000ULL) >> 56 ) );
-        this->write( (unsigned char)( (value & 0x00FF000000000000ULL) >> 48 ) );
-        this->write( (unsigned char)( (value & 0x0000FF0000000000ULL) >> 40 ) );
-        this->write( (unsigned char)( (value & 0x000000FF00000000ULL) >> 32 ) );
-        this->write( (unsigned char)( (value & 0x00000000FF000000ULL) >> 24 ) );
-        this->write( (unsigned char)( (value & 0x0000000000FF0000ULL) >> 16 ) );
-        this->write( (unsigned char)( (value & 0x000000000000FF00ULL) >> 8 ) );
-        this->write( (unsigned char)( (value & 0x00000000000000FFULL) >> 0 ) );
+        unsigned char buffer[sizeof(value)];
+
+        buffer[0] = (value & 0xFF00000000000000ULL) >> 56;
+        buffer[1] = (value & 0x00FF000000000000ULL) >> 48;
+        buffer[2] = (value & 0x0000FF0000000000ULL) >> 40;
+        buffer[3] = (value & 0x000000FF00000000ULL) >> 32;
+        buffer[4] = (value & 0x00000000FF000000ULL) >> 24;
+        buffer[5] = (value & 0x0000000000FF0000ULL) >> 16;
+        buffer[6] = (value & 0x000000000000FF00ULL) >> 8;
+        buffer[7] = (value & 0x00000000000000FFULL) >> 0;
+
+        outputStream->write( buffer, sizeof(value) );
+        written += sizeof( value );
     }
     AMQ_CATCH_RETHROW( IOException )
     AMQ_CATCHALL_THROW( IOException )
