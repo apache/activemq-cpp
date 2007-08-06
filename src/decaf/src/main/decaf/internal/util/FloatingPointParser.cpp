a11ef1f2d2d13ca2a8fb3b6cb6f3e2d7497276cd
Timothy A. Bish <tabish@apache.org>
2007-08-06 10:17:04 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-103
    Adding in more Types wrappers
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@563092 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/decaf/src/main/decaf/internal/util/FloatingPointParser.cpp
@@ -0,0 +1,460 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "FloatingPointParser.h"
+#include <decaf/lang/Math.h>
+#include <decaf/lang/Integer.h>
+#include <decaf/lang/Long.h>
+#include <decaf/lang/Float.h>
+#include <decaf/lang/Double.h>
+#include <decaf/lang/Character.h>
+#include <decaf/internal/util/HexStringParser.h>
+#include <apr_lib.h>
+
+using namespace std;
+using namespace decaf;
+using namespace decaf::lang;
+using namespace decaf::internal;
+using namespace decaf::internal::util;
+
+////////////////////////////////////////////////////////////////////////////////
+double FloatingPointParser::parseDblImpl( const std::string& value, int exp )
+    throw ( exceptions::NumberFormatException ) {
+
+    // assumes s is a null terminated string with at least one
+    // character in it
+    LONG_UNION def[17];
+    LONG_UNION defBackup[17];
+    LONG_UNION* f;
+    LONG_UNION* fNoOverflow;
+    unsigned long long* g;
+    unsigned long long* tempBackup;
+    unsigned int overflow = 0;
+    double result = 0.0;
+    int index = 1;
+    int unprocessedDigits = 0;
+    std::string::const_iterator valItr = value.begin();
+
+    f = def;
+    fNoOverflow = defBackup;
+    (*f).longValue = 0;
+    tempBackup = g = 0;
+
+    do {
+
+        if( *valItr >= '0' && *valItr <= '9' ) {
+
+            // Make a back up of f before appending, so that we can
+            // back out of it if there is no more room, i.e. index >
+            // MAX_ACCURACY_WIDTH.
+
+            memcpy( fNoOverflow, f, sizeof(unsigned long long) * index );
+            overflow = simpleAppendDecimalDigitHighPrecision( f, index, *valItr - '0' );
+//            if( overflow ) {
+//                f[index++] = overflow;
+//                /* There is an overflow, but there is no more room
+//                 * to store the result. We really only need the top 52
+//                 * bits anyway, so we must back out of the overflow,
+//                 * and ignore the rest of the string.
+//                 */
+//                if (index >= MAX_ACCURACY_WIDTH) {
+//                  index--;
+//                  memcpy (f, fNoOverflow, sizeof (U_64) * index);
+//                  break;
+//                }
+//
+//                if (tempBackup) {
+//                  fNoOverflow = tempBackup;
+//                }
+//            }
+//        } else {
+//            index = -1;
+//        }
+
+    } while( index > 0 && *(++valItr) != '\0' );
+//
+//    /* We've broken out of the parse loop either because we've reached
+//     * the end of the string or we've overflowed the maximum accuracy
+//     * limit of a double. If we still have unprocessed digits in the
+//     * given string, then there are three possible results:
+//     *   1. (unprocessed digits + e) == 0, in which case we simply
+//     *      convert the existing bits that are already parsed
+//     *   2. (unprocessed digits + e) < 0, in which case we simply
+//     *      convert the existing bits that are already parsed along
+//     *      with the given e
+//     *   3. (unprocessed digits + e) > 0 indicates that the value is
+//     *      simply too big to be stored as a double, so return Infinity
+//     */
+//    if( ( unprocessedDigits = strlen (s) ) > 0 ) {
+//
+//        e += unprocessedDigits;
+//        if( index > -1 ) {
+//
+//            if (e == 0) {
+//                result = toDoubleHighPrecision (f, index);
+//            }
+//            else if (e < 0) {
+//                result = createDouble1 (env, f, index, e);
+//            } else {
+//                DOUBLE_TO_LONGBITS (result) = INFINITE_LONGBITS;
+//            }
+//        } else {
+//
+//            LOW_I32_FROM_VAR( result ) = -1;
+//            HIGH_I32_FROM_VAR( result ) = -1;
+//        }
+//    } else {
+//
+//        if( index > -1 ) {
+//            if( e == 0 ) {
+//                result = toDoubleHighPrecision( f, index );
+//            } else {
+//                result = createDouble1( env, f, index, e );
+//            } else {
+//                LOW_I32_FROM_VAR( result ) = -1;
+//                HIGH_I32_FROM_VAR( result ) = -1;
+//            }
+//        }
+//    }
+
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+float FloatingPointParser::parseFltImpl( const std::string& value, int exp )
+    throw ( exceptions::NumberFormatException ) {
+
+    return 0.0f;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+FloatingPointParser::StringExponentPair FloatingPointParser::initialParse(
+    const std::string& value, int length ) throw ( exceptions::NumberFormatException ){
+
+    std::string newValue = value;
+    bool negative = false;
+    char c;
+    int start = 0;
+    int end = 0;
+    int decimal = 0;
+    int e = 0;
+
+    if( length == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::initialParse - Invalid zero length string");
+    }
+
+    c = newValue[length - 1];
+    if( c == 'D' || c == 'd' || c == 'F' || c == 'f') {
+        length--;
+        if( length == 0 ) {
+            throw exceptions::NumberFormatException(
+                __FILE__, __LINE__,
+                "FloatingPointParser::initialParse - Invalid zero length string");
+        }
+    }
+
+    end = Math::max( (int)newValue.find_first_of( 'E' ),
+                     (int)newValue.find_first_of( 'e' ) );
+
+    if( end > -1 ) {
+
+        if( end + 1 == length ) {
+            if( length == 0 ) {
+                throw exceptions::NumberFormatException(
+                    __FILE__, __LINE__,
+                    "FloatingPointParser::initialParse - Invalid no data after e",
+                    newValue.c_str() );
+            }
+        }
+
+        int exponent_offset = end + 1;
+
+        if( newValue[exponent_offset] == '+' ) {
+            if( newValue[exponent_offset + 1] == '-' ) {
+                if( length == 0 ) {
+                    throw exceptions::NumberFormatException(
+                        __FILE__, __LINE__,
+                        "FloatingPointParser::initialParse - "
+                        "Invalid length string");
+                }
+            }
+            exponent_offset++; // skip the plus sign
+        }
+
+        try {
+            e = Integer::parseInt( newValue.substr( exponent_offset, length ) );
+        } catch( exceptions::NumberFormatException ex ) {
+            // ex contains the exponent substring only so throw a
+            // new exception with the correct string
+            throw exceptions::NumberFormatException(
+                __FILE__, __LINE__,
+                "FloatingPointParser::initialParse - "
+                "exponent string is not valid" );
+        }
+
+    } else {
+        end = length;
+    }
+
+    if( length == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::initialParse - "
+            "invalid length string", newValue.c_str() );
+    }
+
+    c = newValue[start];
+
+    if( c == '-') {
+        ++start;
+        --length;
+        negative = true;
+    } else if( c == '+' ) {
+        ++start;
+        --length;
+    }
+
+    if( length == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::initialParse - "
+            "invalid length string", newValue.c_str() );
+    }
+
+    decimal = newValue.find_first_of( '.' );
+    if( decimal > -1 ) {
+        e -= end - decimal - 1;
+        newValue = newValue.substr( start, decimal ) + newValue.substr( decimal + 1, end );
+    } else {
+        newValue = newValue.substr( start, end );
+    }
+
+    if( ( length = newValue.length() ) == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::initialParse - "
+            "invalid length string", newValue.c_str() );
+    }
+
+    end = length;
+    while( end > 1 && newValue[end - 1] == '0' ) {
+        --end;
+    }
+
+    start = 0;
+    while( start < end - 1 && newValue[start] == '0' ) {
+        start++;
+    }
+
+    if( end != length || start != 0 ) {
+        e += length - end;
+        newValue = newValue.substr( start, end );
+    }
+
+    return StringExponentPair( newValue, e, negative );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+double FloatingPointParser::parseDblName(
+    const std::string& namedDouble, int length )
+        throw( exceptions::NumberFormatException ) {
+
+    // Valid strings are only +Nan, NaN, -Nan, +Infinity, Infinity,
+    // -Infinity.
+    if( (length != 3) && ( length != 4) && ( length != 8) && ( length != 9 ) ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::parseDblName - "
+            "invalid name string", namedDouble.c_str() );
+    }
+
+    bool negative = namedDouble.at(0) == '-' ? true : false;
+
+    if( namedDouble.find_first_of( "Infinity" ) != string::npos ) {
+        return negative ? Double::NEGATIVE_INFINITY : Float::POSITIVE_INFINITY;
+    }
+
+    if( namedDouble.find_first_of( "NaN" ) != string::npos ) {
+        return Double::NaN;
+    }
+
+    throw exceptions::NumberFormatException(
+        __FILE__, __LINE__,
+        "FloatingPointParser::parseDblName - "
+        "invalid name string", namedDouble.c_str() );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+float FloatingPointParser::parseFltName(
+    const std::string& namedFloat, int length )
+        throw( exceptions::NumberFormatException ) {
+
+    // Valid strings are only +Nan, NaN, -Nan, +Infinity, Infinity,
+    // -Infinity.
+    if( ( length != 3 ) && ( length != 4 ) && ( length != 8 ) && ( length != 9 ) ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::parseFltName - "
+            "invalid name string", namedFloat.c_str() );
+    }
+
+    bool negative = namedFloat.at(0) == '-' ? true : false;
+
+    if( namedFloat.find_first_of( "Infinitiy" ) != string::npos ) {
+        return negative ? Float::NEGATIVE_INFINITY : Float::POSITIVE_INFINITY;
+    }
+
+    if( namedFloat.find_first_of( "NaN" ) != string::npos ) {
+        return Float::NaN;
+    }
+
+    throw exceptions::NumberFormatException(
+        __FILE__, __LINE__,
+        "FloatingPointParser::parseFltName - "
+        "invalid name string", namedFloat.c_str() );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+double FloatingPointParser::parseDouble( const std::string& value )
+    throw( exceptions::NumberFormatException ) {
+
+    std::string newValue = value;
+    FloatingPointParser::trim( newValue );
+    int length = newValue.length();
+
+    if( length == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::parseDouble - "
+            "invalid length string", value.c_str() );
+    }
+
+    // See if this could be a named double
+    char last = newValue[length - 1];
+
+    if( (last == 'y') || (last == 'N') ) {
+        return parseDblName( newValue, length );
+    }
+
+    // See if it could be a hexadecimal representation
+    if( toLowerCase( newValue ).find_first_of( "0x" ) != string::npos ) {
+        return HexStringParser::parseDouble(value);
+    }
+
+    StringExponentPair info = initialParse(value, length);
+
+    double result = parseDblImpl( info.value, info.exp );
+
+    if( info.negative ) {
+        result = -result;
+    }
+
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+float FloatingPointParser::parseFloat( const std::string& value )
+    throw( exceptions::NumberFormatException ) {
+
+    std::string newValue = value;
+    FloatingPointParser::trim( newValue );
+
+    int length = newValue.length();
+
+    if( length == 0 ) {
+        throw exceptions::NumberFormatException(
+            __FILE__, __LINE__,
+            "FloatingPointParser::parseFloat - "
+            "invalid length string", value.c_str() );
+    }
+
+    // See if this could be a named float
+    char last = newValue[length - 1];
+    if( (last == 'y') || (last == 'N') ) {
+        return parseFltName( value, length );
+    }
+
+    // See if it could be a hexadecimal representation
+    if( toLowerCase( newValue ).find_first_of( "0x" ) != string::npos ) {
+        return HexStringParser::parseFloat( newValue );
+    }
+
+    StringExponentPair info = initialParse( newValue, length );
+
+    float result = parseFltImpl( info.value, info.exp );
+    if( info.negative ) {
+        result = -result;
+    }
+
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::string& FloatingPointParser::trim( std::string& value ) {
+
+    char const* delims = " \t\r\n";
+
+    // trim leading whitespace
+    string::size_type notwhite = value.find_first_not_of( delims );
+    value.erase( 0, notwhite );
+
+    // trim trailing whitespace
+    notwhite = value.find_last_not_of( delims );
+    value.erase( notwhite+1 );
+
+    return value;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::string& FloatingPointParser::toLowerCase( std::string& value ) {
+
+    string::size_type index = 0;
+
+    for( ; index < value.size(); ++index ) {
+        if( Character::isLetter( value[index] ) ) {
+            value[index] = apr_tolower( value[index] );
+        }
+    }
+
+    return value;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+unsigned int FloatingPointParser::simpleAppendDecimalDigitHighPrecision(
+    LONG_UNION* arg1, int length, unsigned long long digit )
+{
+    // assumes digit is less than 32 bits
+    unsigned long long arg;
+    IDATA index = 0;
+
+    digit <<= 32;
+
+    do {
+        arg = LOW_IN_U64 (arg1[index]);
+        digit = HIGH_IN_U64 (digit) + TIMES_TEN (arg);
+        LOW_U32_FROM_PTR (arg1 + index) = LOW_U32_FROM_VAR (digit);
+
+        arg = HIGH_IN_U64 (arg1[index]);
+        digit = HIGH_IN_U64 (digit) + TIMES_TEN (arg);
+        HIGH_U32_FROM_PTR (arg1 + index) = LOW_U32_FROM_VAR (digit);
+    }
+    while( ++index < length );
+
+    return HIGH_U32_FROM_VAR( digit );
+}
---
+++ src/decaf/src/main/decaf/internal/util/FloatingPointParser.h
@@ -0,0 +1,164 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef _DECAF_INTERNAL_UTIL_FLOATINGPOINTPARSER_H_
+#define _DECAF_INTERNAL_UTIL_FLOATINGPOINTPARSER_H_
+
+#include <decaf/util/Config.h>
+#include <decaf/lang/exceptions/NumberFormatException.h>
+#include <string>
+
+namespace decaf{
+namespace internal{
+namespace util{
+
+    /**
+     * Used to parse a string and return either a single or double precision
+     * floating point number.
+     */
+    class FloatingPointParser {
+    private:
+
+        class StringExponentPair {
+        public:
+
+            std::string value;
+            int exp;
+            bool negative;
+
+            StringExponentPair( const std::string value, int exp, bool negative ) {
+                this->value = value;
+                this->exp = exp;
+                this->negative = negative;
+            }
+        };
+
+        // Used to make masking easier
+        typedef union {
+            unsigned long long longValue;
+            unsigned int intValue[2];
+            double doubleValue;
+        } LONG_UNION;
+
+    public:
+
+        virtual ~FloatingPointParser() {}
+
+    public:
+
+        /**
+         * Returns the closest double value to the real number in the string.
+         * @param value - the String that will be parsed to a floating point
+         * @return the double closest to the real number
+         * @exception NumberFormatException if value is not a valid Number
+         */
+        static double parseDouble( const std::string& value )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /**
+         * Returns the closest float value to the real number in the string.
+         * @param value - the String that will be parsed to a floating point
+         * @return the float closest to the real number
+         * @exception NumberFormatException if value is not a valid Number
+         */
+        static float parseFloat( const std::string& value )
+            throw ( lang::exceptions::NumberFormatException );
+
+    private:
+
+        /**
+         * Takes a String and an integer exponent. The String should hold a positive
+         * integer value (or zero). The exponent will be used to calculate the
+         * floating point number by taking the positive integer the String
+         * represents and multiplying by 10 raised to the power of the of the
+         * exponent. Returns the closest double value to the real number
+         * @param value - the String that will be parsed to a floating point
+         * @param exp - an int represent the 10 to part
+         * @return the double closest to the real number
+         * @exception NumberFormatException if string isn't a valid number
+         */
+        static double parseDblImpl( const std::string& value, int exp )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /**
+         * Takes a String and an integer exponent. The String should hold a positive
+         * integer value (or zero). The exponent will be used to calculate the
+         * floating point number by taking the positive integer the String
+         * represents and multiplying by 10 raised to the power of the of the
+         * exponent. Returns the closest float value to the real number
+         * @param value - the String that will be parsed to a floating point
+         * @param exp - an int represent the 10 to part
+         * @return the float closest to the real number
+         * @exception NumberFormatException fi the string is not a valid number
+         */
+        static float parseFltImpl( const std::string& value, int exp )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /**
+         * Takes a String and does some initial parsing. Should return a
+         * StringExponentPair containing a String with no leading or trailing white
+         * space and trailing zeroes eliminated. The exponent of the
+         * StringExponentPair will be used to calculate the floating point number by
+         * taking the positive integer the String represents and multiplying by 10
+         * raised to the power of the of the exponent.
+         * @param value - the String that will be parsed to a floating point
+         * @param length - the length of s
+         * @return a StringExponentPair with necessary values
+         * @exception NumberFormatException
+         */
+        static StringExponentPair initialParse( const std::string& value, int length )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /*
+         * Assumes the string is trimmed.
+         * @param value - the String that will be parsed to a floating point
+         * @param length - the length of s
+         * @return the parsed double
+         * @exception NumberFormatException
+         */
+        static double parseDblName( const std::string& namedDouble, int length )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /*
+         * Assumes the string is trimmed.
+         * @param value - the String that will be parsed to a floating point
+         * @param length - the length of s
+         * @return the parsed double
+         * @exception NumberFormatException
+         */
+        static float parseFltName( const std::string& namedFloat, int length )
+            throw ( lang::exceptions::NumberFormatException );
+
+        /**
+         * Trims the leading and trailing whitespace from a string
+         * @param value - reference to the string to trim
+         * @param returns a reference to the string passed for chaining
+         */
+        static std::string& trim( std::string& value );
+
+        /**
+         * Turns all letters in the string into lowercase.
+         * @param value - reference to the string to trim
+         * @param returns a reference to the string passed for chaining
+         */
+        static std::string& toLowerCase( std::string& value );
+
+    };
+
+}}}
+
+#endif /*_DECAF_INTERNAL_UTIL_FLOATINGPOINTPARSER_H_*/
