cd6744254f33c4657e669f36ed9586212eb0308a
Timothy A. Bish <tabish@apache.org>
2007-08-09 00:00:02 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-103
    Adding in more Types wrappers
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@564048 13f79535-47bb-0310-9956-ffa450edef68

--- src/decaf/src/main/decaf/internal/util/BigInt.h
+++ src/decaf/src/main/decaf/internal/util/BigInt.h
@@ -43,6 +43,28 @@ namespace util{
             static const int LoWord = 0;
         #endif
 
+        static const long long TEN_E1 = 0xALL;
+        static const long long TEN_E2 = 0x64LL;
+        static const long long TEN_E3 = 0x3E8LL;
+        static const long long TEN_E4 = 0x2710LL;
+        static const long long TEN_E5 = 0x186A0LL;
+        static const long long TEN_E6 = 0xF4240LL;
+        static const long long TEN_E7 = 0x989680LL;
+        static const long long TEN_E8 = 0x5F5E100LL;
+        static const long long TEN_E9 = 0x3B9ACA00LL;
+        static const long long TEN_E19 = 0x8AC7230489E80000LL;
+
+        static const int E_OFFSET = 1075;
+
+        static const unsigned long long MANTISSA_MASK = 0x000FFFFFFFFFFFFFULL;
+        static const unsigned long long EXPONENT_MASK = 0x7FF0000000000000ULL;
+        static const unsigned long long NORMAL_MASK = 0x0010000000000000ULL;
+        static const unsigned long long SIGN_MASK = 0x8000000000000000ULL;
+        static const unsigned int FLOAT_MANTISSA_MASK = 0x007FFFFF;
+        static const unsigned int FLOAT_EXPONENT_MASK = 0x7F800000;
+        static const unsigned int FLOAT_NORMAL_MASK = 0x00800000;
+        static const unsigned int FLOAT_E_OFFSET = 150;
+
     public:
 
         BigInt();
@@ -71,25 +93,48 @@ namespace util{
                                          unsigned long long* arg2,
                                          int length2 );
 
-        static int highestSetBitHighPrecision(unsigned long long* arg, int length );
-        static void subtractHighPrecision(unsigned long long* arg1, int length1, unsigned long long* arg2,
-                                    int length2);
-        static int doubleExponent(double z);
-        static unsigned int simpleMultiplyHighPrecision(unsigned long long* arg1, int length, unsigned long long arg2 );
-        static int addHighPrecision(unsigned long long * arg1, int length1, unsigned long long* arg2,
-                                int length2);
-        static void simpleMultiplyAddHighPrecisionBigEndianFix(unsigned long long* arg1, int length,
-                                                         unsigned long long arg2, unsigned int* result );
-        static int lowestSetBit(unsigned long long* y);
-        static int timesTenToTheEHighPrecision(unsigned long long * result, int length, jint e );
-        static void simpleMultiplyAddHighPrecision(unsigned long long * arg1, int length, unsigned long long arg2,
-                                             unsigned int * result);
-        static int highestSetBit(unsigned long long * y );
-        static int lowestSetBitHighPrecision(unsigned long long* arg, int length );
-        static void simpleShiftLeftHighPrecision(unsigned long long* arg1, int length, int arg2 );
+        static int highestSetBitHighPrecision( unsigned long long* arg, int length );
+
+        static void subtractHighPrecision( unsigned long long* arg1,
+                                           int length1,
+                                           unsigned long long* arg2,
+                                           int length2 );
+
+        static int doubleExponent( double z );
+
+        static unsigned int simpleMultiplyHighPrecision(
+            unsigned long long* arg1, int length, unsigned long long arg2 );
+
+        static int addHighPrecision( unsigned long long* arg1,
+                                     int length1,
+                                     unsigned long long* arg2,
+                                     int length2 );
+
+        static int lowestSetBit( unsigned long long* y );
+
+        static int timesTenToTheEHighPrecision(
+            unsigned long long* result, int length, int e );
+
+        static void simpleMultiplyAddHighPrecision(
+            unsigned long long* arg1, int length,
+            unsigned long long arg2, unsigned int* result );
+
+        static int highestSetBit( unsigned long long* y );
+
+        static int lowestSetBitHighPrecision( unsigned long long* arg, int length );
+
+        static void simpleShiftLeftHighPrecision( unsigned long long* arg1,
+                                                  int length, int arg2 );
+
         static unsigned int floatMantissa( float z );
-        static unsigned long long simpleMultiplyHighPrecision64( unsigned long long* arg1, int length, unsigned long long arg2 );
-        static int simpleAddHighPrecision( unsigned long long* arg1, int length, unsigned long long arg2 );
+
+        static unsigned long long simpleMultiplyHighPrecision64(
+            unsigned long long* arg1, int length, unsigned long long arg2 );
+
+        static int simpleAddHighPrecision( unsigned long long* arg1,
+                                           int length,
+                                           unsigned long long arg2 );
+
         static int floatExponent( float z );
 
     };
