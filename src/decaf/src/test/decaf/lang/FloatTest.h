26f763b5597431be82dd2b756e0edc38bae4fa03
Timothy A. Bish <tabish@apache.org>
2007-08-11 16:40:25 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-103
    Adding in more Types wrappers
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@564937 13f79535-47bb-0310-9956-ffa450edef68

--- src/decaf/src/test/decaf/lang/FloatTest.h
+++ src/decaf/src/test/decaf/lang/FloatTest.h
@@ -29,15 +29,81 @@ namespace lang{
         CPPUNIT_TEST_SUITE( FloatTest );
         CPPUNIT_TEST( test_ConstructorF );
         CPPUNIT_TEST( test_ConstructorString );
+        CPPUNIT_TEST( test_byteValue );
+        CPPUNIT_TEST( test_compare );
+        CPPUNIT_TEST( test_doubleValue );
+        CPPUNIT_TEST( test_floatToIntBitsF );
+        CPPUNIT_TEST( test_floatToRawIntBitsF );
+        CPPUNIT_TEST( test_floatValue );
+        CPPUNIT_TEST( test_intBitsToFloatI );
+        CPPUNIT_TEST( test_intValue );
+        CPPUNIT_TEST( test_isInfinite );
+        CPPUNIT_TEST( test_isInfiniteF );
+        CPPUNIT_TEST( test_isNaN );
+        CPPUNIT_TEST( test_isNaNF );
+        CPPUNIT_TEST( test_longValue );
+        CPPUNIT_TEST( test_parseFloatLDecaf_lang_String );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
+//        CPPUNIT_TEST( test_byteValue );
         CPPUNIT_TEST_SUITE_END();
 
     public:
+
+        static const int rawBitsFor3_4eN38To38[];
+        static const std::string expectedStringFor3_4eN38To38[];
+        static const int rawBitsFor1_17eN38To38[];
+        static const std::string expectedStringFor1_17eN38To38[];
 
         FloatTest();
         virtual ~FloatTest() {}
 
         void test_ConstructorF();
         void test_ConstructorString();
+        void test_byteValue();
+        void test_compare();
+        void test_doubleValue();
+        void test_floatToIntBitsF();
+        void test_floatToRawIntBitsF();
+        void test_floatValue();
+        void test_intBitsToFloatI();
+        void test_intValue();
+        void test_isInfinite();
+        void test_isInfiniteF();
+        void test_isNaN();
+        void test_isNaNF();
+        void test_longValue();
+        void test_parseFloatLDecaf_lang_String();
+
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+//        void test_byteValue();
+
+    private:
+
+        void doTestCompareRawBits( const std::string& originalFloatString,
+                                   int expectedRawBits,
+                                   const std::string& expectedString );
 
     };
 
