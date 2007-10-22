25c9a5c5885388ac9d72a5b06c88b603492f45f6
Timothy A. Bish <tabish@apache.org>
2007-10-22 17:04:59 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-142
    Cleaning up the marshaling test code.
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@587155 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/activemq/connector/openwire/marshal/BaseDataStreamMarshallerTest.cpp
+++ src/test/activemq/connector/openwire/marshal/BaseDataStreamMarshallerTest.cpp
@@ -41,10 +41,10 @@ using namespace activemq::connector::openwire::marshal;
 
 ////////////////////////////////////////////////////////////////////////////////
 void BaseDataStreamMarshallerTest::testLooseMarshal()
-{   
+{
     SimpleDataStructureMarshaller* simpleMarshaller = new SimpleDataStructureMarshaller();
     ComplexDataStructureMarshaller* complexMarshaller = new ComplexDataStructureMarshaller();
-    Properties props;    
+    Properties props;
     OpenWireFormat openWireFormat(props);
     openWireFormat.addMarshaller( simpleMarshaller );
     openWireFormat.addMarshaller( complexMarshaller );
@@ -52,19 +52,19 @@ void BaseDataStreamMarshallerTest::testLooseMarshal()
     // Marshal the dataStructure to a byte array.
     ByteArrayOutputStream baos;
     DataOutputStream looseOut( &baos );
-    looseOut.writeByte( dataStructure->getDataStructureType() );    
+    looseOut.writeByte( dataStructure->getDataStructureType() );
     complexMarshaller->looseMarshal( &openWireFormat, dataStructure, &looseOut );
-    
+
     // Now read it back in and make sure it's all right.
     ByteArrayInputStream bais( baos.getByteArray(), baos.getByteArraySize() );
     DataInputStream looseIn( &bais );
-    
+
     unsigned char dataType = looseIn.readByte();
     CPPUNIT_ASSERT( dataType == dataStructure->getDataStructureType() );
 
     ComplexDataStructure ds;
     complexMarshaller->looseUnmarshal( &openWireFormat, &ds, &looseIn );
-    
+
     CPPUNIT_ASSERT( ds.boolValue == dataStructure->boolValue );
     CPPUNIT_ASSERT( ds.cachedChild != NULL );
     CPPUNIT_ASSERT( ds.cachedChild->boolValue == dataStructure->cachedChild->boolValue );
@@ -78,10 +78,10 @@ void BaseDataStreamMarshallerTest::testLooseMarshal()
 
 ////////////////////////////////////////////////////////////////////////////////
 void BaseDataStreamMarshallerTest::testTightMarshal()
-{   
+{
     SimpleDataStructureMarshaller* simpleMarshaller = new SimpleDataStructureMarshaller();
     ComplexDataStructureMarshaller* complexMarshaller = new ComplexDataStructureMarshaller();
-    Properties props;    
+    Properties props;
     OpenWireFormat openWireFormat(props);
     openWireFormat.addMarshaller( simpleMarshaller );
     openWireFormat.addMarshaller( complexMarshaller );
@@ -89,23 +89,22 @@ void BaseDataStreamMarshallerTest::testTightMarshal()
     // Marshal the dataStructure to a byte array.
     ByteArrayOutputStream baos;
     DataOutputStream dataOut( &baos );
-    
+
     // Phase 1 - count the size
     int size = 1;
     BooleanStream bs;
     size += complexMarshaller->tightMarshal1( &openWireFormat, dataStructure, &bs );
     size += bs.marshalledSize();
-    
+
     // Phase 2 - marshal
     dataOut.writeByte( dataStructure->getDataStructureType() );
     bs.marshal( &dataOut );
     complexMarshaller->tightMarshal2( &openWireFormat, dataStructure, &dataOut, &bs );
-    
-    
+
     // Now read it back in and make sure it's all right.
     ByteArrayInputStream bais( baos.getByteArray(), baos.getByteArraySize() );
     DataInputStream dataIn( &bais );
-    
+
     unsigned char dataType = dataIn.readByte();
     CPPUNIT_ASSERT( dataType == dataStructure->getDataStructureType() );
 
@@ -113,7 +112,7 @@ void BaseDataStreamMarshallerTest::testTightMarshal()
     bs.clear();
     bs.unmarshal( &dataIn );
     complexMarshaller->tightUnmarshal( &openWireFormat, &ds, &dataIn, &bs );
-    
+
     CPPUNIT_ASSERT( ds.boolValue == dataStructure->boolValue );
     CPPUNIT_ASSERT( ds.cachedChild != NULL );
     CPPUNIT_ASSERT( ds.cachedChild->boolValue == dataStructure->cachedChild->boolValue );
@@ -122,8 +121,5 @@ void BaseDataStreamMarshallerTest::testTightMarshal()
     CPPUNIT_ASSERT( ds.cachedChild->intValue == dataStructure->cachedChild->intValue );
     CPPUNIT_ASSERT( ds.cachedChild->floatValue == dataStructure->cachedChild->floatValue );
     CPPUNIT_ASSERT( ds.cachedChild->doubleValue == dataStructure->cachedChild->doubleValue );
-    printf("string1[%s,%d] string2[%s,%d]\n",ds.cachedChild->stringValue.c_str(), (int)ds.cachedChild->stringValue.size(), dataStructure->cachedChild->stringValue.c_str(), (int)dataStructure->cachedChild->stringValue.size() );
     CPPUNIT_ASSERT( ds.cachedChild->stringValue == dataStructure->cachedChild->stringValue );
 }
-
-
