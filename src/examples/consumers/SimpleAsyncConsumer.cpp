ae2bf44cf75b795a307f67f4fe6b47a001d6c389
Timothy A. Bish <tabish@apache.org>
2007-10-26 16:05:58 +0000

    Adding more examples for the CPP client, this is a simple async consumer example.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@588694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/examples/consumers/SimpleAsyncConsumer.cpp
@@ -0,0 +1,231 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <activemq/concurrent/Thread.h>
+#include <activemq/concurrent/Runnable.h>
+#include <activemq/concurrent/CountDownLatch.h>
+#include <activemq/core/ActiveMQConnectionFactory.h>
+#include <activemq/util/Integer.h>
+#include <activemq/util/Config.h>
+#include <activemq/util/Date.h>
+#include <cms/Connection.h>
+#include <cms/Session.h>
+#include <cms/TextMessage.h>
+#include <cms/BytesMessage.h>
+#include <cms/MapMessage.h>
+#include <cms/ExceptionListener.h>
+#include <cms/MessageListener.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace activemq;
+using namespace activemq::core;
+using namespace activemq::util;
+using namespace activemq::concurrent;
+using namespace cms;
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+class SimpleAsyncConsumer : public ExceptionListener,
+                            public MessageListener {
+private:
+
+    Connection* connection;
+    Session* session;
+    Destination* destination;
+    MessageConsumer* consumer;
+    bool useTopic;
+    std::string brokerURI;
+    std::string destURI;
+
+public:
+
+    SimpleAsyncConsumer( const std::string& brokerURI,
+                         const std::string& destURI,
+                         bool useTopic = false ) {
+        connection = NULL;
+        session = NULL;
+        destination = NULL;
+        consumer = NULL;
+        this->useTopic = useTopic;
+        this->brokerURI = brokerURI;
+        this->destURI = destURI;
+    }
+
+    virtual ~SimpleAsyncConsumer(){
+        cleanup();
+    }
+
+    void runConsumer() {
+
+        try {
+
+            // Create a ConnectionFactory
+            ActiveMQConnectionFactory* connectionFactory =
+                new ActiveMQConnectionFactory( brokerURI );
+
+            // Create a Connection
+            connection = connectionFactory->createConnection();
+            delete connectionFactory;
+            connection->start();
+
+            connection->setExceptionListener(this);
+
+            // Create a Session
+            session = connection->createSession( Session::AUTO_ACKNOWLEDGE );
+
+            // Create the destination (Topic or Queue)
+            if( useTopic ) {
+                destination = session->createTopic( destURI );
+            } else {
+                destination = session->createQueue( destURI );
+            }
+
+            // Create a MessageConsumer from the Session to the Topic or Queue
+            consumer = session->createConsumer( destination );
+            consumer->setMessageListener( this );
+
+        } catch (CMSException& e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Called from the consumer since this class is a registered MessageListener.
+    virtual void onMessage( const Message* message ){
+
+        static int count = 0;
+
+        try
+        {
+            count++;
+            const TextMessage* textMessage =
+                dynamic_cast< const TextMessage* >( message );
+            string text = "";
+
+            if( textMessage != NULL ) {
+                text = textMessage->getText();
+            } else {
+                text = "NOT A TEXTMESSAGE!";
+            }
+
+            printf( "Message #%d Received: %s\n", count, text.c_str() );
+        } catch (CMSException& e) {
+            e.printStackTrace();
+        }
+    }
+
+    // If something bad happens you see it here as this class is also been
+    // registered as an ExceptionListener with the connection.
+    virtual void onException( const CMSException& ex AMQCPP_UNUSED) {
+        printf("CMS Exception occured.  Shutting down client.\n");
+    }
+
+private:
+
+    void cleanup(){
+
+        //*************************************************
+        // Always close destination, consumers and producers before
+        // you destroy their sessions and connection.
+        //*************************************************
+
+        // Destroy resources.
+        try{
+            if( destination != NULL ) delete destination;
+        }catch (CMSException& e) { e.printStackTrace(); }
+        destination = NULL;
+
+        try{
+            if( consumer != NULL ) delete consumer;
+        }catch (CMSException& e) { e.printStackTrace(); }
+        consumer = NULL;
+
+        // Close open resources.
+        try{
+            if( session != NULL ) session->close();
+            if( connection != NULL ) connection->close();
+        }catch (CMSException& e) { e.printStackTrace(); }
+
+        // Now Destroy them
+        try{
+            if( session != NULL ) delete session;
+        }catch (CMSException& e) { e.printStackTrace(); }
+        session = NULL;
+
+        try{
+            if( connection != NULL ) delete connection;
+        }catch (CMSException& e) { e.printStackTrace(); }
+        connection = NULL;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc AMQCPP_UNUSED, char* argv[] AMQCPP_UNUSED) {
+
+    std::cout << "=====================================================\n";
+    std::cout << "Starting the example:" << std::endl;
+    std::cout << "-----------------------------------------------------\n";
+
+    // Set the URI to point to the IPAddress of your broker.
+    // add any optional params to the url to enable things like
+    // tightMarshalling or tcp logging etc.  See the CMS website for
+    // a full list of configuration options.
+    //
+    //  http://activemq.apache.org/cms/
+    //
+    // Wire Foormat Options:
+    // =====================
+    // Use either stomp or openwire, the default ports are different for each
+    //
+    // Examples:
+    //    tcp://127.0.0.1:61616                      default to openwire
+    //    tcp://127.0.0.1:61616?wireFormat=openwire  same as above
+    //    tcp://127.0.0.1:61613?wireFormat=stomp     use stomp instead
+    //
+    std::string brokerURI =
+        "tcp://127.0.0.1:61616"
+        "?wireFormat=openwire"
+        "&transport.useAsyncSend=true";
+//        "&transport.commandTracingEnabled=true"
+//        "&transport.tcpTracingEnabled=true";
+//        "&wireFormat.tightEncodingEnabled=true";
+
+
+    //============================================================
+    // This is the Destination Name and URI options.  Use this to
+    // customize where the consumer listens, to have the consumer
+    // use a topic or queue set the 'useTopics' flag.
+    //============================================================
+    std::string destURI = "TEST.FOO";
+
+    //============================================================
+    // set to true to use topics instead of queues
+    // Note in the code above that this causes createTopic or
+    // createQueue to be used in both consumer an producer.
+    //============================================================
+    bool useTopics = true;
+
+    // Create the consumer
+    SimpleAsyncConsumer consumer( brokerURI, destURI, useTopics );
+
+    // Start it up and it will listen forever.
+    consumer.runConsumer();
+
+    // Wait to exit.
+    std::cout << "Press 'q' to quit" << std::endl;
+    while( std::cin.get() != 'q') {}
+}
