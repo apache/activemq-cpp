c996f64976633292e92fedee9c80c47e0c728487
Nathan Christopher Mittler <nmittler@apache.org>
2008-01-08 03:20:55 +0000

    AMQCPP-152 - Adding classes for support of CmsTemplate
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@609859 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/activemq/cmsutil/PooledSession.h
@@ -0,0 +1,184 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ACTIVEMQ_CMSUTIL_POOLEDSESSION_H_
+#define ACTIVEMQ_CMSUTIL_POOLEDSESSION_H_
+
+#include <cms/Session.h>
+
+namespace activemq {
+namespace cmsutil {
+
+    // Forward declarations.
+    class SessionPool;
+
+    /**
+     * A pooled session object that wraps around a delegate session.  Calls
+     * to close this session only result in giving the session back to the pool.
+     */
+    class PooledSession : public cms::Session {
+    private:
+        
+        SessionPool* pool;
+        
+        cms::Session* session;
+        
+    public:
+        
+    	PooledSession( SessionPool* pool, cms::Session* session );
+    	
+    	/**
+    	 * Does nothing
+    	 */
+    	virtual ~PooledSession();
+
+    	/**
+         * Returns a non-constant reference to the internal session object.
+         * 
+         * @return the session object.
+         */
+    	virtual cms::Session* getSession() {
+    	    return session;
+    	}
+    	
+    	/**
+    	 * Returns a constant reference to the internal session object.
+    	 * 
+    	 * @return the session object.
+    	 */
+    	virtual const cms::Session* getSession() const {
+            return session;
+        }
+    	
+    	/**
+    	 * Returns this session back to the pool, but does not close 
+    	 * or destroy the internal session object.
+    	 */
+        virtual void close() throw( cms::CMSException );
+        
+        virtual void commit() throw ( cms::CMSException ) {
+            session->commit();
+        }
+        
+        virtual void rollback() throw ( cms::CMSException ) {
+            session->rollback();
+        }
+        
+        virtual cms::MessageConsumer* createConsumer(
+            const cms::Destination* destination )
+                throw ( cms::CMSException ) {
+            return session->createConsumer(destination);
+        }
+        
+        virtual cms::MessageConsumer* createConsumer( 
+            const cms::Destination* destination,
+            const std::string& selector )
+                throw ( cms::CMSException ) {
+            return session->createConsumer(destination, selector);
+        }
+        
+        virtual cms::MessageConsumer* createConsumer( 
+            const cms::Destination* destination,
+            const std::string& selector,
+            bool noLocal )
+                throw ( cms::CMSException ) {
+            return session->createConsumer(destination, selector, noLocal);
+        }
+        
+        virtual cms::MessageConsumer* createDurableConsumer(
+            const cms::Topic* destination,
+            const std::string& name,
+            const std::string& selector,
+            bool noLocal = false )
+                throw ( cms::CMSException ) {
+            return session->createDurableConsumer(destination, name, selector, noLocal);
+        }
+        
+        virtual cms::MessageProducer* createProducer( const cms::Destination* destination )
+            throw ( cms::CMSException ) {
+            return session->createProducer(destination);
+        }
+        
+        virtual cms::Queue* createQueue( const std::string& queueName )
+            throw ( cms::CMSException ) {
+            return session->createQueue(queueName);
+        }
+        
+        virtual cms::Topic* createTopic( const std::string& topicName )
+            throw ( cms::CMSException ) {
+            return session->createTopic(topicName);
+        }
+
+        virtual cms::TemporaryQueue* createTemporaryQueue()
+            throw ( cms::CMSException ) {
+            return session->createTemporaryQueue();
+        }
+
+        virtual cms::TemporaryTopic* createTemporaryTopic()
+            throw ( cms::CMSException ) {
+            return session->createTemporaryTopic();
+        }
+
+        virtual cms::Message* createMessage() 
+            throw ( cms::CMSException ) {            
+            return session->createMessage();
+        }
+
+        virtual cms::BytesMessage* createBytesMessage() 
+            throw ( cms::CMSException) {            
+            return session->createBytesMessage();
+        }
+        
+        virtual cms::BytesMessage* createBytesMessage(
+            const unsigned char* bytes,
+            std::size_t bytesSize ) 
+                throw ( cms::CMSException) {            
+            return session->createBytesMessage(bytes, bytesSize);
+        }
+        
+        virtual cms::TextMessage* createTextMessage() 
+            throw ( cms::CMSException ) {
+            return session->createTextMessage();
+        }
+
+        virtual cms::TextMessage* createTextMessage( const std::string& text ) 
+            throw ( cms::CMSException ) {
+            return session->createTextMessage(text);
+        }
+        
+        virtual cms::MapMessage* createMapMessage() 
+            throw ( cms::CMSException ) {
+            return session->createMapMessage();
+        }
+        
+        virtual cms::Session::AcknowledgeMode getAcknowledgeMode() const {
+            return session->getAcknowledgeMode();
+        }
+        
+        virtual bool isTransacted() const {
+            return session->isTransacted();
+        }
+
+        virtual void unsubscribe( const std::string& name ) 
+            throw ( cms::CMSException ) {
+            session->unsubscribe(name);
+        }
+    };
+
+}}
+
+#endif /*ACTIVEMQ_CMSUTIL_POOLEDSESSION_H_*/
