61d4c60e28db6325d407993078a8a68f1b30c6cf
Timothy A. Bish <tabish@apache.org>
2008-08-27 22:04:38 +0000

    Add a start on the Atomic Package
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@689641 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/decaf/util/concurrent/atomic/AtomicBoolean.cpp
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AtomicBoolean.h"
+
+#include <decaf/lang/Boolean.h>
+
+using namespace decaf;
+using namespace decaf::lang;
+using namespace decaf::util;
+using namespace decaf::util::concurrent;
+using namespace decaf::util::concurrent::atomic;
+
+////////////////////////////////////////////////////////////////////////////////
+AtomicBoolean::AtomicBoolean() {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool AtomicBoolean::compareAndSet( bool expect, bool update ) {
+    // TODO
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool AtomicBoolean::getAndSet( bool newValue ) {
+    for(;;) {
+        bool current = get();
+        if( compareAndSet( current, newValue ) ) {
+            return current;
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::string AtomicBoolean::toString() {
+    return Boolean::toString( this->value ? true : false );
+}
