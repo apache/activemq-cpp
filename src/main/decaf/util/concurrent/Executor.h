b531fbaecdee15713b9e3f583f7e08121cd5ba1e
Timothy A. Bish <tabish@apache.org>
2008-11-08 03:25:03 +0000

    Add some additional interfaces to Decaf
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@712348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/decaf/util/concurrent/Executor.h
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _DECAF_UTIL_CONCURRENT_EXECUTOR_H_
+#define _DECAF_UTIL_CONCURRENT_EXECUTOR_H_
+
+#include <decaf/util/Config.h>
+
+#include <decaf/lang/Runnable.h>
+#include <decaf/lang/exceptions/NullPointerException.h>
+#include <decaf/util/concurrent/RejectedExecutionException.h>
+
+namespace decaf {
+namespace util {
+namespace concurrent {
+
+    /**
+     * An object that executes submitted {@link Runnable} tasks. This
+     * interface provides a way of decoupling task submission from the
+     * mechanics of how each task will be run, including details of thread
+     * use, scheduling, etc.  An <tt>Executor</tt> is normally used
+     * instead of explicitly creating threads. For example, rather than
+     * invoking <tt>new Thread(new(RunnableTask())).start()</tt> for each
+     * of a set of tasks, you might use:
+     *
+     * <pre>
+     * Executor executor = <em>anExecutor</em>;
+     * executor->execute( new RunnableTask1() );
+     * executor->execute( new RunnableTask2() );
+     * ...
+     * </pre>
+     *
+     * However, the <tt>Executor</tt> interface does not strictly
+     * require that execution be asynchronous. In the simplest case, an
+     * executor can run the submitted task immediately in the caller's
+     * thread:
+     *
+     * <pre>
+     * class DirectExecutor : public Executor {
+     * public:
+     *
+     *     void execute( Runnable* r ) {
+     *         r->run();
+     *     }
+     *
+     * }</pre>
+     *
+     * More typically, tasks are executed in some thread other
+     * than the caller's thread.  The executor below spawns a new thread
+     * for each task.
+     *
+     * <pre>
+     * class ThreadPerTaskExecutor : public Executor {
+     * public:
+     *     std::vector&lt;Thread*gt; threads;
+     *
+     *     void execute( Runnable* r ) {
+     *         threads.push_back( new Thread( r ) );
+     *         threads.rbegin()->start();
+     *     }
+     *
+     * }</pre>
+     *
+     * The <tt>Executor</tt> implementations provided in this package
+     * implement {@link ExecutorService}, which is a more extensive
+     * interface.  The {@link ThreadPoolExecutor} class provides an
+     * extensible thread pool implementation. The {@link Executors} class
+     * provides convenient factory methods for these Executors.
+     *
+     * @since 1.0
+     */
+    class Executor {
+    public:
+
+        virtual ~Executor() {}
+
+        /**
+         * Executes the given command at some time in the future.  The command
+         * may execute in a new thread, in a pooled thread, or in the calling
+         * thread, at the discretion of the <tt>Executor</tt> implementation.
+         *
+         * @param command the runnable task
+         *
+         * @throws RejectedExecutionException if this task cannot be
+         *         accepted for execution.
+         *
+         * @throws NullPointerException if command is null
+         */
+        virtual void execute( Runnable* command )
+            throw ( decaf::util::concurrent::RejectedExecutionException,
+                    decaf::lang::exceptions::NullPointerException ) = 0;
+
+    };
+
+}}}
+
+#endif /* _DECAF_UTIL_CONCURRENT_EXECUTOR_H_ */
---
+++ src/main/decaf/util/concurrent/ThreadFactory.h
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _DECAF_UTIL_CONCURRENT_THREADFACTORY_H_
+#define _DECAF_UTIL_CONCURRENT_THREADFACTORY_H_
+
+namespace decaf {
+namespace util {
+namespace concurrent {
+
+    /**
+     * public interface ThreadFactory
+     *
+     * An object that creates new threads on demand. Using thread factories removes
+     * hardwiring of calls to new Thread, enabling applications to use special thread
+     * subclasses, priorities, etc.
+     *
+     * The simplest implementation of this interface is just:
+     *
+     *    class SimpleThreadFactory : public ThreadFactory {
+     *    public:
+     *       Thread* newThread( Runnable* r ) {
+     *          return new Thread(r);
+     *       }
+     *    }
+     *
+     * The Executors.defaultThreadFactory() method provides a more useful simple
+     * implementation, that sets the created thread context to known values before
+     * returning it.
+     *
+     * @since 1.0
+     */
+    class ThreadFactory {
+    public:
+
+        virtual ~ThreadFactory() {}
+
+        /**
+         * Constructs a new Thread. Implementations may also initialize priority, name,
+         * daemon status, ThreadGroup, etc.
+         *
+         * @param r - a runnable to be executed by new thread instance
+         *
+         * @returns constructed thread, or null if the request to create a thread is rejected
+         */
+        virtual Thread* newThread( Runnable* r ) = 0;
+
+    };
+
+}}}
+
+#endif /* _DECAF_UTIL_CONCURRENT_THREADFACTORY_H_ */
