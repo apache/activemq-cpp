85a3a84fb582d24bb2e1b5a313046277dcb691fa
Timothy A. Bish <tabish@apache.org>
2009-01-23 02:01:42 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-100
    The initial skeleton classes for the Failover Transport.
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@736889 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/activemq/transport/failover/FailoverTransport.h
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORT_H_
+#define _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORT_H_
+
+#include <activemq/util/Config.h>
+
+#include <activemq/transport/TransportFilter.h>
+
+namespace activemq {
+namespace transport {
+namespace failover {
+
+    class BackupTransport;
+
+    class AMQCPP_API FailoverTransport : public TransportFilter {
+    public:
+
+        FailoverTransport();
+        virtual ~FailoverTransport();
+
+    };
+
+}}}
+
+#endif /* _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORT_H_ */
---
+++ src/main/activemq/transport/failover/FailoverTransportFactory.h
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORTFACTORY_H_
+#define _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORTFACTORY_H_
+
+#include <activemq/util/Config.h>
+
+#include <activemq/transport/AbstractTransportFactory.h>
+#include <activemq/transport/Transport.h>
+#include <activemq/exceptions/ActiveMQException.h>
+#include <activemq/wireformat/WireFormat.h>
+#include <decaf/net/URI.h>
+#include <decaf/util/Properties.h>
+
+namespace activemq {
+namespace transport {
+namespace failover {
+
+    /**
+     * Creates an instance of a FailoverTransport.
+     *
+     * @since 3.0
+     */
+    class AMQCPP_API FailoverTransportFactory : public AbstractTransportFactory {
+    public:
+
+        FailoverTransportFactory();
+        virtual ~FailoverTransportFactory();
+
+        /**
+         * Creates a slimed down Transport instance which can be used in composite
+         * transport instances.
+         * @param location - URI location to connect to.
+         * @param wireformat - the assigned WireFormat for the new Transport.
+         * @param properties - Properties to apply to the transport.
+         * @throws ActiveMQexception if an error occurs
+         */
+        virtual Transport* doCreateComposite( const decaf::net::URI& location,
+                                              wireformat::WireFormat* wireFormat,
+                                              const decaf::util::Properties& properties )
+            throw ( exceptions::ActiveMQException );
+
+    };
+
+}}}
+
+#endif /* _ACTIVE_TRANSPORT_FAILOVER_FAILOVERTRANSPORTFACTORY_H_ */
