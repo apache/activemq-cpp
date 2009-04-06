3a451b8bed219d1cc9f3d8856e2a907a747c29be
Timothy A. Bish <tabish@apache.org>
2009-04-06 00:07:15 +0000

    Change the copy and assignment generator code to fix a compiler warning on Mac.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@762189 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp-openwire-generator/src/main/java/org/apache/activemq/openwire/tool/commands/CommandHeaderGenerator.java
+++ activemq-cpp-openwire-generator/src/main/java/org/apache/activemq/openwire/tool/commands/CommandHeaderGenerator.java
@@ -66,8 +66,8 @@ public class CommandHeaderGenerator extends CommandCodeGenerator {
         if( !isAssignable() ) {
             out.println("    protected:");
             out.println("");
-            out.println("        "+getClassName()+"( const "+getClassName()+"& other AMQCPP_UNUSED ) {};");
-            out.println("        "+getClassName()+"& operator= ( const "+getClassName()+"& other AMQCPP_UNUSED ) { return *this; };");
+            out.println("        "+getClassName()+"( const "+getClassName()+"& ) : " + getBaseClassName() + "() {};");
+            out.println("        "+getClassName()+"& operator= ( const "+getClassName()+"& ) { return *this; };");
             out.println("");
         }
 
