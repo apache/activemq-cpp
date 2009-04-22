ea3a97e134d6198eaad14d6670175e1a5ff7f8b1
Timothy A. Bish <tabish@apache.org>
2009-04-22 18:57:53 +0000

    http://issues.apache.org/activemq/browse/AMQCPP-238
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/branches/activemq-cpp-2.x@767619 13f79535-47bb-0310-9956-ffa450edef68

--- src/test-integration/main.cpp
+++ src/test-integration/main.cpp
@@ -17,31 +17,74 @@
 
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestListener.h>
 #include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/Outputter.h>
+#include <cppunit/XmlOutputter.h>
 #include <cppunit/TestResult.h>
+#include <util/teamcity/TeamCityProgressListener.h>
 #include <activemq/util/Config.h>
+#include <decaf/lang/Runtime.h>
+#include <decaf/lang/Integer.h>
 #include <iostream>
+#include <memory>
 
-int main( int argc AMQCPP_UNUSED, char **argv AMQCPP_UNUSED)
-{
-    try
-    {
+int main( int argc AMQCPP_UNUSED, char **argv AMQCPP_UNUSED ) {
+
+    bool wasSuccessful = false;
+    std::ofstream outputFile;
+    bool useXMLOutputter = false;
+    std::auto_ptr<CppUnit::TestListener> listener( new CppUnit::BriefTestProgressListener );
+
+    if( argc > 1 ) {
+        for( int i = 1; i < argc; ++i ) {
+            const std::string arg( argv[i] );
+            if( arg == "-teamcity" ) {
+                listener.reset( new test::util::teamcity::TeamCityProgressListener() );
+            } else if( arg == "-quiet" ) {
+                listener.reset( NULL );
+            } else if( arg == "-xml" ) {
+                if( ( i + 1 ) >= argc ) {
+                    std::cout << "-xml requires a filename to be specified" << std::endl;
+                    return -1;
+                }
+
+                std::ofstream outputFile( argv[++i] );
+                useXMLOutputter = true;
+            }
+        }
+    }
+
+    try {
+
         CppUnit::TextUi::TestRunner runner;
         CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
         runner.addTest( registry.makeTest() );
-    
+
         // Shows a message as each test starts
-        CppUnit::BriefTestProgressListener listener;
-        runner.eventManager().addListener( &listener );
-        
+        if( listener.get() != NULL ) {
+            runner.eventManager().addListener( listener.get() );
+        }
+
+        // Specify XML output and inform the test runner of this format.  The TestRunner
+        // will delete the passed XmlOutputter for us.
+        if( useXMLOutputter ) {
+            runner.setOutputter( new CppUnit::XmlOutputter( &runner.result(), outputFile ) );
+        }
+
         bool wasSuccessful = runner.run( "", false );
+
+        if( useXMLOutputter ) {
+            outputFile.close();
+        }
+
         return !wasSuccessful;
     }
     catch(...) {
         std::cout << "----------------------------------------" << std::endl;
         std::cout << "- AN ERROR HAS OCCURED:                -" << std::endl;
         std::cout << "- Do you have a Broker Running?        -" << std::endl;
-        std::cout << "----------------------------------------" << std::endl;        
+        std::cout << "----------------------------------------" << std::endl;
     }
 }
 
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -17,40 +17,80 @@
 
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestListener.h>
 #include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/Outputter.h>
+#include <cppunit/XmlOutputter.h>
 #include <cppunit/TestResult.h>
+#include <util/teamcity/TeamCityProgressListener.h>
 #include <activemq/util/Config.h>
 #include <decaf/lang/Runtime.h>
 #include <decaf/lang/Integer.h>
 #include <iostream>
+#include <memory>
 
 int main( int argc, char **argv ) {
 
-    // ensure that we start the runtime.
-    decaf::lang::Runtime::getRuntime();
     bool wasSuccessful = false;
     int iterations = 1;
+    std::ofstream outputFile;
+    bool useXMLOutputter = false;
+    std::auto_ptr<CppUnit::TestListener> listener( new CppUnit::BriefTestProgressListener );
 
     if( argc > 1 ) {
-        try {
-            iterations = decaf::lang::Integer::parseInt( argv[1] );
-        } catch( decaf::lang::exceptions::NumberFormatException& ex ) {
-            iterations = 1;
+        for( int i = 1; i < argc; ++i ) {
+            const std::string arg( argv[i] );
+            if( arg == "-runs" ) {
+                if( ( i + 1 ) >= argc ) {
+                    std::cout << "-runs requires a value for the iteration count" << std::endl;
+                    return -1;
+                }
+                try {
+                    iterations = decaf::lang::Integer::parseInt( argv[++i] );
+                } catch( decaf::lang::exceptions::NumberFormatException& ex ) {
+                    std::cout << "Invalid iteration count specified on command line: "
+                              << argv[i] << std::endl;
+                    return -1;
+                }
+            } else if( arg == "-teamcity" ) {
+                listener.reset( new test::util::teamcity::TeamCityProgressListener() );
+            } else if( arg == "-quiet" ) {
+                listener.reset( NULL );
+            } else if( arg == "-xml" ) {
+                if( ( i + 1 ) >= argc ) {
+                    std::cout << "-xml requires a filename to be specified" << std::endl;
+                    return -1;
+                }
+
+                std::ofstream outputFile( argv[++i] );
+                useXMLOutputter = true;
+            }
         }
     }
 
     for( int i = 0; i < iterations; ++i ) {
+
         CppUnit::TextUi::TestRunner runner;
         CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
         runner.addTest( registry.makeTest() );
 
         // Shows a message as each test starts
-        CppUnit::BriefTestProgressListener listener;
-        runner.eventManager().addListener( &listener );
+        if( listener.get() != NULL ) {
+            runner.eventManager().addListener( listener.get() );
+        }
+
+        // Specify XML output and inform the test runner of this format.  The TestRunner
+        // will delete the passed XmlOutputter for us.
+        if( useXMLOutputter ) {
+            runner.setOutputter( new CppUnit::XmlOutputter( &runner.result(), outputFile ) );
+        }
 
         wasSuccessful = runner.run( "", false );
+
+        if( useXMLOutputter ) {
+            outputFile.close();
+        }
     }
 
     return !wasSuccessful;
 }
-
