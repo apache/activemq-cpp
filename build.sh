147704eb4a61f9674bff661064c07460a7bfbfcf
Timothy A. Bish <tabish@apache.org>
2009-05-19 21:36:56 +0000

    More tweaks for the build process.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@776478 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -16,3 +16,86 @@
 # limitations under the License.
 # ------------------------------------------------------------------------
 
+# default settings
+runautogen=true
+runconfigure=true
+maketarget=all
+
+# load the user supplied build configuration file if present.
+if [ -f "./build.conf" ] ; then
+  . ./build.conf
+fi
+
+# No Configure script means we have to run autogen, which in turn means
+# we need to run a new Configure on the project.
+if [ -x "./configure" ] ; then
+
+    # even if we have a configure script, if the template has been updated we should
+    # run it again to account for the changes, which also means we need to run a autogen
+    # to create the actual script and associated autoconf artifacts.
+    if [ "./configure.ac" -ot "./configure" ] ; then
+        echo "No need to run autogen.sh or configure"
+        runautgen=false
+        runconfigure=false
+    else
+        echo "We need to run autogen.sh and configure"
+        runautgen=true
+        runconfigure=true
+    fi
+
+fi
+
+run_Configure {
+    autogen.sh
+    pushd build
+    ../configure
+    exit
+}
+
+run_Clean() {
+    pushd build
+    make clean
+    exit
+}
+
+run_Make() {
+    pushd build
+    make
+    exit
+}
+
+run_Check() {
+    pushd build
+    make check
+    exit
+}
+
+run_Doxygen() {
+    pushd build
+    make doxygen-run
+    exit
+}
+
+case "$1" in
+configure)
+    run_Configure
+    ;;
+all)
+    run_Make
+    ;;
+check)
+    run_Check
+    ;;
+clean)
+    run_Clean
+    ;;
+doxygen)
+    run_Doxygen
+    ;;
+*)
+    echo "Usage: $0 {clean}"
+    exit 1
+esac
+
+exit 0
+
