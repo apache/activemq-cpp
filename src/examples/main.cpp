8c10f628b17bb6fe7c4cab42d838581c349a0e63
Timothy A. Bish <tabish@apache.org>
2009-06-10 14:09:18 +0000

    Samples use Failover Transport by default now.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@783365 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -357,14 +357,14 @@ int main(int argc AMQCPP_UNUSED, char* argv[] AMQCPP_UNUSED) {
     //    tcp://127.0.0.1:61613?wireFormat=stomp     use stomp instead
     //
     std::string brokerURI =
-        "tcp://127.0.0.1:61616"
-        "?wireFormat=openwire"
+        "failover:(tcp://127.0.0.1:61616"
+//        "?wireFormat=openwire"
 //        "&connection.alwaysSyncSend=true"
 //        "&connection.useAsyncSend=true"
 //        "&transport.commandTracingEnabled=true"
 //        "&transport.tcpTracingEnabled=true"
 //        "&wireFormat.tightEncodingEnabled=true"
-        "";
+        ")";
 
     //============================================================
     // set to true to use topics instead of queues
