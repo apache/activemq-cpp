195c605a6f5f4cd78d4f0ec8d53c43e360fdd1a2
Timothy A. Bish <tabish@apache.org>
2009-06-12 23:22:04 +0000

    Add a new Sample to demonstrate how to deal with Advisory Topics.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@784311 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-cpp/src/examples/advisories/AdvisoryProducer.h
@@ -0,0 +1,72 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ACTIVEMQCPP_EXAMPLES_ADVISORIES_ADVISORYPRODUCER_H_
+#define _ACTIVEMQCPP_EXAMPLES_ADVISORIES_ADVISORYPRODUCER_H_
+
+#include <string>
+#include <memory>
+
+#include <cms/Session.h>
+#include <cms/MessageProducer.h>
+#include <cms/MessageConsumer.h>
+#include <cms/MessageListener.h>
+
+#include <decaf/lang/Runnable.h>
+#include <decaf/util/concurrent/CountDownLatch.h>
+
+namespace activemqcpp {
+namespace examples {
+namespace advisories {
+
+    class AdvisoryProducer : public decaf::lang::Runnable,
+                             public cms::MessageListener {
+    private:
+
+        volatile bool consumerOnline;
+        volatile bool shutdown;
+        decaf::util::concurrent::CountDownLatch shutdownLatch;
+
+        cms::Session* session;
+        std::auto_ptr<cms::MessageConsumer> consumer;
+        std::auto_ptr<cms::MessageProducer> producer;
+
+    public:
+
+        AdvisoryProducer( cms::Session* session );
+        virtual ~AdvisoryProducer();
+
+        /**
+         * Shut down the processing that occurs in the Run method.
+         */
+        void stop();
+
+        /**
+         * Run the producer code.
+         */
+        virtual void run();
+
+        /**
+         * Async Message callback.
+         */
+        virtual void onMessage( const cms::Message* message );
+
+    };
+
+}}}
+
+#endif /* _ACTIVEMQCPP_EXAMPLES_ADVISORIES_ADVISORYPRODUCER_H_ */
