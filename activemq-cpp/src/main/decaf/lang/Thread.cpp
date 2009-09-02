0ef714ec122a959611039ff972eb601e6232f9b6
Timothy A. Bish <tabish@apache.org>
2009-09-02 22:55:42 +0000

    Refactored thread class implementation hides all the implementation details from the user, no more dependency on APR includes or macros in the header.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@810727 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/main/decaf/lang/Thread.cpp
+++ activemq-cpp/src/main/decaf/lang/Thread.cpp
@@ -21,12 +21,15 @@
 #pragma warning( disable: 4311 )
 #endif
 
+#include <apr.h>
 #include <apr_time.h>
 #include <apr_portable.h>
+#include <apr_thread_proc.h>
 
 #include <decaf/internal/DecafRuntime.h>
 #include <decaf/lang/Exception.h>
 #include <decaf/lang/exceptions/RuntimeException.h>
+#include <decaf/lang/exceptions/NullPointerException.h>
 
 using namespace decaf;
 using namespace decaf::internal;
@@ -34,29 +37,98 @@ using namespace decaf::lang;
 using namespace decaf::lang::exceptions;
 
 ////////////////////////////////////////////////////////////////////////////////
+namespace decaf{
+namespace lang{
+
+    class ThreadProperties {
+    public:
+
+        /**
+         * The task to be run by this thread, defaults to
+         * this thread object.
+         */
+        Runnable* task;
+
+        /**
+         * APR Thread Handle
+         */
+        apr_thread_t* threadHandle;
+
+        /**
+         * Started state of this thread.
+         */
+        bool started;
+
+        /**
+         * Indicates whether the thread has already been
+         * joined.
+         */
+        bool joined;
+
+    public:
+
+        ThreadProperties() : task( NULL ), threadHandle( NULL ), started( false ), joined( false ) {
+        }
+
+        ThreadProperties( Runnable* task ) : task( task ), threadHandle( NULL ), started( false ), joined( false ) {
+        }
+
+        static void* APR_THREAD_FUNC runCallback( apr_thread_t* self, void* param ) {
+
+            // Get the instance.
+            ThreadProperties* properties = (ThreadProperties*)param;
+
+            // Invoke run on the task.
+            try{
+                properties->task->run();
+            } catch( ... ){
+                RuntimeException ex(
+                    __FILE__, __LINE__,
+                    "unhandled exception bubbled up to Thread::run");
+                ex.printStackTrace();
+            }
+
+            // Indicate we are done.
+            apr_thread_exit( self, APR_SUCCESS );
+            return NULL;
+        }
+
+    };
+
+}}
+
+////////////////////////////////////////////////////////////////////////////////
 Thread::Thread() {
-    this->task = this;
-    this->started = false;
-    this->joined = false;
-    this->threadHandle = NULL;
+
+    this->initialize( this );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 Thread::Thread( Runnable* task ) {
-    this->task = task;
-    this->started = false;
-    this->joined = false;
-    this->threadHandle = NULL;
+
+    if( task == NULL ) {
+        throw NullPointerException(
+            __FILE__, __LINE__,
+            "Thread::Thread( Runnable* ) Runnable instance passed was NULL" );
+    }
+
+    this->initialize( task );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-Thread::~Thread(){
+void Thread::initialize( Runnable* task ) {
+
+    this->properties.reset( new ThreadProperties( task ) );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+Thread::~Thread() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void Thread::start() throw ( Exception )
 {
-    if( this->started ) {
+    if( this->properties->started ) {
         throw Exception(
             __FILE__, __LINE__,
             "Thread::start - Thread already started");
@@ -65,10 +137,10 @@ void Thread::start() throw ( Exception )
     DecafRuntime* runtime = dynamic_cast<DecafRuntime*>( Runtime::getRuntime() );
 
     apr_status_t err = apr_thread_create(
-        &this->threadHandle,
+        &this->properties->threadHandle,
         NULL,
-        runCallback,
-        this,
+        this->properties->runCallback,
+        this->properties.get(),
         runtime->getGlobalPool() );
 
     if( err != APR_SUCCESS ) {
@@ -78,25 +150,26 @@ void Thread::start() throw ( Exception )
     }
 
     // Mark the thread as started.
-    started = true;
+    this->properties->started = true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 void Thread::join() throw( Exception )
 {
-    if( !this->started ) {
+    if( !this->properties->started ) {
         throw Exception( __FILE__, __LINE__,
             "Thread::join() called without having called Thread::start()");
     }
 
-    if( !this->joined ) {
+    if( !this->properties->joined ) {
         apr_status_t threadReturn;
-        if( apr_thread_join( &threadReturn, this->threadHandle ) != APR_SUCCESS ) {
+        if( apr_thread_join( &threadReturn, this->properties->threadHandle ) != APR_SUCCESS ) {
             throw Exception( __FILE__, __LINE__,
                 "Thread::join() - Failed to Join the Thread");
         }
     }
-    this->joined = true;
+
+    this->properties->joined = true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -112,25 +185,4 @@ void Thread::yield() {
 ////////////////////////////////////////////////////////////////////////////////
 unsigned long Thread::getId() {
     return (unsigned long)( apr_os_thread_current() );
-}
-
-////////////////////////////////////////////////////////////////////////////////
-void* APR_THREAD_FUNC Thread::runCallback( apr_thread_t* self, void* param ) {
-
-    // Get the instance.
-    Thread* thread = (Thread*)param;
-
-    // Invoke run on the task.
-    try{
-        thread->task->run();
-    } catch( ... ){
-        RuntimeException ex(
-            __FILE__, __LINE__,
-            "unhandled exception bubbled up to Thread::run");
-        ex.printStackTrace();
-    }
-
-    // Indicate we are done.
-    apr_thread_exit( self, APR_SUCCESS );
-    return NULL;
 }
