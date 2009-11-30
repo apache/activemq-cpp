9fcdd8cb13f026160a12af5fabb53aeefd38696c
Timothy A. Bish <tabish@apache.org>
2009-11-30 22:13:17 +0000

    Adds additional tests to the Individual Acknowledge tests
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@885594 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/test-integration/activemq/test/openwire/OpenwireIndividualAckTest.cpp
+++ activemq-cpp/src/test-integration/activemq/test/openwire/OpenwireIndividualAckTest.cpp
@@ -146,3 +146,143 @@ void OpenwireIndividualAckTest::testUnAckedMessageAreNotConsumedOnSessionClose()
 
     session->close();
 }
+
+////////////////////////////////////////////////////////////////////////////////
+void OpenwireIndividualAckTest::testIndividualAcknowledgeMultiMessages_AcknowledgeFirstTest()
+{
+    Connection* connection = this->cmsProvider->getConnection();
+    connection->start();
+
+    std::auto_ptr<Session> session( connection->createSession( cms::Session::INDIVIDUAL_ACKNOWLEDGE ) );
+    std::auto_ptr<Destination> queue( session->createTemporaryQueue() );
+    std::auto_ptr<MessageProducer> producer( session->createProducer( queue.get() ) );
+
+    std::auto_ptr<TextMessage> msg1( session->createTextMessage("test 1") );
+    producer->send( msg1.get() );
+    std::auto_ptr<TextMessage> msg2( session->createTextMessage("test 2") );
+    producer->send( msg2.get() );
+
+    producer->close();
+
+    std::auto_ptr<MessageConsumer> consumer( session->createConsumer( queue.get() ) );
+
+    // Read the first message
+    std::auto_ptr<Message> recvMsg1( consumer->receive( 2000 ) );
+    CPPUNIT_ASSERT( recvMsg1.get() != NULL );
+    CPPUNIT_ASSERT( msg1->getText() == dynamic_cast<TextMessage*>( recvMsg1.get() )->getText() );
+
+    // Read the second message
+    std::auto_ptr<Message> recvMsg2( consumer->receive( 2000 ) );
+    CPPUNIT_ASSERT( recvMsg2.get() != NULL );
+    CPPUNIT_ASSERT( msg2->getText() == dynamic_cast<TextMessage*>( recvMsg2.get() )->getText() );
+
+    // Acknowledge first message
+    recvMsg1->acknowledge();
+
+    consumer->close();
+
+    // Read first message a second time
+    consumer.reset( session->createConsumer( queue.get() ) );
+    std::auto_ptr<Message> recvMsg3( consumer->receive( 2000 ) );
+    CPPUNIT_ASSERT( recvMsg3.get() != NULL );
+    CPPUNIT_ASSERT( msg2->getText() == dynamic_cast<TextMessage*>( recvMsg3.get() )->getText() );
+
+    // Try to read second message a second time
+    std::auto_ptr<Message> recvMsg4( consumer->receive( 2000 ) );
+    CPPUNIT_ASSERT( recvMsg4.get() == NULL );
+
+    consumer->close();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void OpenwireIndividualAckTest::testManyMessageAckedAfterMessageConsumption()
+{
+    int messageCount = 20;
+    std::auto_ptr<Message> msg;
+
+    Connection* connection = this->cmsProvider->getConnection();
+    connection->start();
+
+    std::auto_ptr<Session> session( connection->createSession( cms::Session::INDIVIDUAL_ACKNOWLEDGE ) );
+    std::auto_ptr<Destination> queue( session->createTemporaryQueue() );
+    std::auto_ptr<MessageProducer> producer( session->createProducer( queue.get() ) );
+
+    for( int i = 0; i < messageCount; i++ )
+    {
+        msg.reset( session->createTextMessage("msg") );
+        producer->send( msg.get() );
+    }
+
+    // Consume the message...
+    std::auto_ptr<MessageConsumer> consumer( session->createConsumer( queue.get() ) );
+
+    for( int i = 0; i < messageCount; i++ )
+    {
+        msg.reset( consumer->receive( 1000 ) );
+        CPPUNIT_ASSERT( msg.get() != NULL );
+        msg->acknowledge();
+    }
+
+    msg.reset( consumer->receive( 1000 ) );
+    CPPUNIT_ASSERT( msg.get() == NULL );
+
+    // Reset the session.
+    session->close();
+    session.reset( connection->createSession( cms::Session::AUTO_ACKNOWLEDGE ) );
+
+    // Attempt to Consume the message...
+    consumer.reset( session->createConsumer( queue.get() ) );
+    msg.reset( consumer->receive( 1000 ) );
+    CPPUNIT_ASSERT( msg.get() == NULL );
+    session->close();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void OpenwireIndividualAckTest::testManyMessageAckedAfterAllConsumption()
+{
+    int messageCount = 20;
+    std::auto_ptr<Message> msg;
+
+    Connection* connection = this->cmsProvider->getConnection();
+    connection->start();
+
+    std::auto_ptr<Session> session( connection->createSession( cms::Session::INDIVIDUAL_ACKNOWLEDGE ) );
+    std::auto_ptr<Destination> queue( session->createTemporaryQueue() );
+    std::auto_ptr<MessageProducer> producer( session->createProducer( queue.get() ) );
+
+    for( int i = 0; i < messageCount; i++ )
+    {
+        msg.reset( session->createTextMessage("msg") );
+        producer->send( msg.get() );
+    }
+
+    // Consume the message...
+    std::auto_ptr<MessageConsumer> consumer( session->createConsumer( queue.get() ) );
+
+    std::vector<Message*> consumedMessages;
+
+    for( int i = 0; i < messageCount; i++ )
+    {
+        Message* message = consumer->receive( 1000 );
+        CPPUNIT_ASSERT( message != NULL );
+        consumedMessages.push_back( message );
+    }
+
+    for( int i = 0; i < messageCount; i++ )
+    {
+        consumedMessages[i]->acknowledge();
+        delete consumedMessages[i];
+    }
+    msg.reset( consumer->receive( 1000 ) );
+    CPPUNIT_ASSERT( msg.get() == NULL );
+
+    // Reset the session.
+    session->close();
+    session.reset( connection->createSession( cms::Session::AUTO_ACKNOWLEDGE ) );
+
+    // Attempt to Consume the message...
+    consumer.reset( session->createConsumer( queue.get() ) );
+    msg.reset( consumer->receive( 1000 ) );
+    CPPUNIT_ASSERT( msg.get() == NULL );
+    session->close();
+}
