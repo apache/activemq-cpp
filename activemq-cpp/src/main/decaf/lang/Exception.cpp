2803e29791e42660897f0765fe6203ad0d7b406e
Timothy A. Bish <tabish@apache.org>
2009-12-08 22:13:42 +0000

    Clean up the code in the setMark function.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/branches/activemq-cpp-3.1.x@888594 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/main/decaf/lang/Exception.cpp
+++ activemq-cpp/src/main/decaf/lang/Exception.cpp
@@ -100,11 +100,11 @@ void Exception::buildMessage( const char* format, va_list& vargs ) {
 void Exception::setMark( const char* file, const int lineNumber ) {
 
     // Add this mark to the end of the stack trace.
-    stackTrace.push_back( std::make_pair( (std::string)file, (int)lineNumber ) );
+    stackTrace.push_back( std::make_pair( std::string( file ), (int)lineNumber ) );
 
-    std::ostringstream stream;
-    stream << "\tFILE: " << stackTrace[stackTrace.size()-1].first;
-    stream << ", LINE: " << stackTrace[stackTrace.size()-1].second;
+    //std::ostringstream stream;
+    //stream << "\tFILE: " << stackTrace[stackTrace.size()-1].first;
+    //stream << ", LINE: " << stackTrace[stackTrace.size()-1].second;
 
     //decaf::util::logger::SimpleLogger logger("com.yadda2");
     //logger.log( stream.str() );
