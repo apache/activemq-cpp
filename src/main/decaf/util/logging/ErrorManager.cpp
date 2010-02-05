3d6b418c40723fa93a090571f2209cbc1bf3eb3d
Timothy A. Bish <tabish@apache.org>
2010-02-05 15:30:22 +0000

    Completed implementation of ErrorManager for Logging package.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@906971 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/decaf/util/logging/ErrorManager.cpp
+++ src/main/decaf/util/logging/ErrorManager.cpp
@@ -17,19 +17,57 @@
 
 #include "ErrorManager.h"
 
+#include <iostream>
+
+using namespace std;
 using namespace decaf;
 using namespace decaf::lang;
 using namespace decaf::util;
 using namespace decaf::util::logging;
 
 ////////////////////////////////////////////////////////////////////////////////
-const int ErrorManager::GENERIC_FAILURE = 1;
-const int ErrorManager::WRITE_FAILURE = 2;
-const int ErrorManager::FLUSH_FAILURE = 3;
-const int ErrorManager::CLOSE_FAILURE = 4;
-const int ErrorManager::OPEN_FAILURE = 5;
-const int ErrorManager::FORMAT_FAILURE = 6;
+const int ErrorManager::GENERIC_FAILURE = 0;
+const int ErrorManager::WRITE_FAILURE = 1;
+const int ErrorManager::FLUSH_FAILURE = 2;
+const int ErrorManager::CLOSE_FAILURE = 3;
+const int ErrorManager::OPEN_FAILURE = 4;
+const int ErrorManager::FORMAT_FAILURE = 5;
+
+////////////////////////////////////////////////////////////////////////////////
+namespace {
+
+    std::string FAILURE_STRINGS[] = {
+       "GENERIC_FAILURE",
+       "WRITE_FAILURE",
+       "FLUSH_FAILURE",
+       "CLOSE_FAILURE",
+       "OPEN_FAILURE",
+       "FORMAT_FAILURE"
+    };
+}
+
+////////////////////////////////////////////////////////////////////////////////
+ErrorManager::ErrorManager() : wasCalled( false ) {
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ErrorManager::~ErrorManager() {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ErrorManager::error( const std::string& message, decaf::lang::Exception* ex, int code ) {
+
+    if( !wasCalled.compareAndSet( false, true ) ) {
+        return;
+    }
+
+    std::cerr << "ErrorManager: " << FAILURE_STRINGS[code] << std::endl;
+
+    if( message != "" ) {
+        std::cerr << "ErrorManager: message = " << message << std::endl;
+    }
+
+    if( ex != NULL ) {
+        std::cerr << "ErrorManager: Exception Message = " << ex->getMessage() << std::endl;
+    }
 }
