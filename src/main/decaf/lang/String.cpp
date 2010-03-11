8dba56c14024f0663209fea5e0f0b5a438c53c5e
Timothy A. Bish <tabish@apache.org>
2010-03-11 22:34:42 +0000

    Implement some more of the String methods, and test.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@922046 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/decaf/lang/String.cpp
+++ src/main/decaf/lang/String.cpp
@@ -17,6 +17,7 @@
 
 #include "String.h"
 
+#include <decaf/lang/ArrayPointer.h>
 #include <decaf/lang/System.h>
 #include <decaf/lang/exceptions/NullPointerException.h>
 
@@ -32,20 +33,16 @@ namespace lang{
     class Contents {
     public:
 
-        unsigned char* value;
+        ArrayPointer<unsigned char> value;
         int length;
         int offset;
 
     public:
 
-        Contents() : value( NULL ), length( 0 ), offset( 0 ) {
+        Contents() : value(), length( 0 ), offset( 0 ) {
         }
 
-        Contents( int length ) : value( new unsigned char[length] ), length( length ), offset( 0 ) {
-        }
-
-        ~Contents() {
-            delete [] value;
+        Contents( int length ) : value( length ), length( length ), offset( 0 ) {
         }
 
     };
@@ -54,23 +51,68 @@ namespace lang{
 
 ////////////////////////////////////////////////////////////////////////////////
 String::String() {
-    this->contents.reset( new Contents() );
+    this->contents = new Contents();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 String::String( const std::string& source ) {
 
     // Initialize the contents object.
-    this->contents.reset( new Contents( source.length() ) );
+    this->contents = new Contents( source.length() );
 
     // load the passed string into the contents value.
-    System::arraycopy( (unsigned char*)source.c_str(), 0, contents->value, 0, source.length() );
+    System::arraycopy( (unsigned char*)source.c_str(), 0, contents->value.get(), 0, source.length() );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 String::~String() {
     try{
+        delete this->contents;
     }
     DECAF_CATCH_NOTHROW( Exception )
     DECAF_CATCHALL_NOTHROW()
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::size_t String::length() const {
+    return this->contents->offset + this->contents->length;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+bool String::isEmpty() const {
+    return ( this->contents->offset + this->contents->length ) == 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+char String::charAt( std::size_t index ) const
+    throw( lang::exceptions::IndexOutOfBoundsException ) {
+
+    try{
+
+        if( index >= this->length() ) {
+            throw IndexOutOfBoundsException(
+                __FILE__, __LINE__, "Index given exceeds length of this String." );
+        }
+
+        return this->contents->value[this->contents->offset + index];
+    }
+    DECAF_CATCH_RETHROW( IndexOutOfBoundsException )
+    DECAF_CATCHALL_THROW( IndexOutOfBoundsException )
+}
+
+////////////////////////////////////////////////////////////////////////////////
+CharSequence* String::subSequence( std::size_t start DECAF_UNUSED, std::size_t end DECAF_UNUSED ) const
+    throw( lang::exceptions::IndexOutOfBoundsException ) {
+
+    return NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+std::string String::toString() const {
+
+    if( this->contents->value == NULL ) {
+        return "null";
+    }
+
+    return std::string( (const char*)this->contents->value.get(), this->length() );
 }
