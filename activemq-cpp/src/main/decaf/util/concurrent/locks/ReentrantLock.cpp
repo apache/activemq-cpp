569b3c3180d6fd3ff80d76eef6402abc40a22af2
Timothy A. Bish <tabish@apache.org>
2010-06-10 11:00:34 +0000

    https://issues.apache.org/activemq/browse/AMQCPP-295
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@953279 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/main/decaf/util/concurrent/locks/ReentrantLock.cpp
+++ activemq-cpp/src/main/decaf/util/concurrent/locks/ReentrantLock.cpp
@@ -486,7 +486,11 @@ long long ConditionObject::awaitNanos( long long nanosTimeout )
     abstime.tv_sec = TimeUnit::NANOSECONDS.toSeconds( delay );
     abstime.tv_nsec = delay % 1000000000;
 
+#ifdef HAVE_PTHREAD_H
     unsigned int result = pthread_cond_timedwait( &condition, &lock->handle, &abstime );
+#else
+
+#endif
 
     // restore the owner
     lock->lock_owner = lock_owner;
