e01b8ee85f0dea509f5a357b3d9fa4bff0119beb
Timothy A. Bish <tabish@apache.org>
2010-12-09 19:09:47 +0000

    Add an optional constructor to all for array init using a specified value.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@1044092 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/decaf/lang/ArrayPointer.h
+++ src/main/decaf/lang/ArrayPointer.h
@@ -25,6 +25,7 @@
 #include <decaf/lang/exceptions/IllegalArgumentException.h>
 #include <decaf/util/concurrent/atomic/AtomicRefCounter.h>
 #include <decaf/util/Comparator.h>
+#include <decaf/util/Arrays.h>
 #include <memory>
 #include <typeinfo>
 #include <algorithm>
@@ -110,6 +111,36 @@ namespace lang {
 
             try{
                 T* value = new T[size];
+                this->array = new ArrayData( value, size );
+            } catch( std::exception& ex ) {
+                REFCOUNTER::release();
+                throw ex;
+            } catch(...) {
+                REFCOUNTER::release();
+                throw std::bad_alloc();
+            }
+        }
+
+        /**
+         * Create a new ArrayPointer instance and allocates an internal array that is sized
+         * using the passed in size value.  The array elements are initialized with the given
+         * value.
+         *
+         * @param size
+         *      The size of the array to allocate for this ArrayPointer instance.
+         * @param fillWith
+         *      The value to initialize each element of the newly allocated array with.
+         */
+        ArrayPointer( int size, const T& fillWith ) :
+            REFCOUNTER(), array( NULL ), onDelete( onDeleteFunc ) {
+
+            if( size == 0 ) {
+                return;
+            }
+
+            try{
+                T* value = new T[size];
+                decaf::util::Arrays::fill( value, size, 0, size, fillWith );
                 this->array = new ArrayData( value, size );
             } catch( std::exception& ex ) {
                 REFCOUNTER::release();
