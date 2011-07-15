0cae6ce0b5d883585d96ef151c06c00e128ac180
Timothy A. Bish <tabish@apache.org>
2011-07-15 21:38:51 +0000

    Add a non-templated marker interface so that non-template code can determine if a particular Runnable is a Future or Callable type and needs special processing.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk@1147324 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-cpp/src/main/decaf/util/concurrent/Future.h
+++ activemq-cpp/src/main/decaf/util/concurrent/Future.h
@@ -18,30 +18,18 @@
 #ifndef _DECAF_UTIL_CONCURRENT_FUTURE_H_
 #define _DECAF_UTIL_CONCURRENT_FUTURE_H_
 
+#include <decaf/util/Config.h>
+
 #include <decaf/util/concurrent/TimeUnit.h>
 
 namespace decaf {
 namespace util {
 namespace concurrent {
 
-    /**
-     * A Future represents the result of an asynchronous computation. Methods
-     * are provided to check if the computation is complete, to wait for its
-     * completion, and to retrieve the result of the computation. The result
-     * can only be retrieved using method get when the computation has
-     * completed, blocking if necessary until it is ready. Cancellation is
-     * performed by the cancel method. Additional methods are provided to
-     * determine if the task completed normally or was canceled. Once a
-     * computation has completed, the computation cannot be canceled. If you
-     * would like to use a Future for the sake of cancellability but not
-     * provide a usable result, you can declare types of the form Future<void*>
-     * and return null as a result of the underlying task.
-     */
-    template<typename V>
-    class Future {
+    class FutureType {
     public:
 
-        virtual ~Future() {}
+        virtual ~FutureType() {}
 
         /**
          * Attempts to cancel execution of this task. This attempt will fail if the
@@ -51,15 +39,17 @@ namespace concurrent {
          * has already started, then the mayInterruptIfRunning parameter determines
          * whether the thread executing this task should be interrupted in an attempt
          * to stop the task.
-         * <p>
+         *
          * After this method returns, subsequent calls to isDone() will always return
          * true. Subsequent calls to isCancelled() will always return true if this
          * method returned true.
-         * <p>
-         * @param mayInterruptIfRunning - true if the thread executing this task should
-         * be interrupted; otherwise, in-progress tasks are allowed to complete.
+         *
+         * @param mayInterruptIfRunning
+         *      True if the thread executing this task should be interrupted; otherwise,
+         *      in-progress tasks are allowed to complete.
+         *
          * @returns false if the task could not be canceled, typically because it has
-         * already completed normally; true otherwise
+         *          already completed normally; true otherwise
          */
         virtual bool cancel( bool mayInterruptIfRunning ) = 0;
 
@@ -76,6 +66,29 @@ namespace concurrent {
          * @returns true if this task completed
          */
         virtual bool isDone() const = 0;
+
+    };
+
+    /**
+     * A Future represents the result of an asynchronous computation. Methods
+     * are provided to check if the computation is complete, to wait for its
+     * completion, and to retrieve the result of the computation. The result
+     * can only be retrieved using method get when the computation has
+     * completed, blocking if necessary until it is ready. Cancellation is
+     * performed by the cancel method. Additional methods are provided to
+     * determine if the task completed normally or was canceled. Once a
+     * computation has completed, the computation cannot be canceled. If you
+     * would like to use a Future for the sake of cancellability but not
+     * provide a usable result, you can declare types of the form Future<void*>
+     * and return null as a result of the underlying task.
+     *
+     * @since 1.0
+     */
+    template<typename V>
+    class Future : public FutureType {
+    public:
+
+        virtual ~Future() {}
 
         /**
          * Waits if necessary for the computation to complete, and then retrieves its result.
