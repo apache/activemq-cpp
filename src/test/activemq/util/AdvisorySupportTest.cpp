d8c0f616c7edadb1a4bfbceea736da9f1eb1c10f
Timothy A. Bish <tabish@apache.org>
2012-05-04 22:37:48 +0000

    Working on Unit Test coverage of AdvisorySupport API
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@1334243 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/activemq/util/AdvisorySupportTest.cpp
+++ src/test/activemq/util/AdvisorySupportTest.cpp
@@ -19,6 +19,8 @@
 
 #include <activemq/util/AdvisorySupport.h>
 #include <activemq/commands/ActiveMQDestination.h>
+#include <activemq/commands/ActiveMQTopic.h>
+#include <activemq/commands/ActiveMQQueue.h>
 
 #include <decaf/lang/Pointer.h>
 
@@ -49,3 +51,274 @@ void AdvisorySupportTest::testGetTempDestinationCompositeAdvisoryTopic() {
     CPPUNIT_ASSERT(topic->getPhysicalName().find(".TempQueue") != std::string::npos);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetAllDestinationsCompositeAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getAllDestinationsCompositeAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".TempTopic") != std::string::npos);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".TempQueue") != std::string::npos);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".Topic") != std::string::npos);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".Queue") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetAllDestinationAdvisoryTopics() {
+
+    Pointer<ActiveMQDestination> target(new ActiveMQTopic("Test.Topic"));
+
+    std::vector<ActiveMQDestination*> topics = AdvisorySupport::getAllDestinationAdvisoryTopics(target.get());
+    CPPUNIT_ASSERT(topics.size() > 0);
+
+    std::vector<ActiveMQDestination*>::iterator iter = topics.begin();
+    for (; iter != topics.end(); ++iter) {
+        Pointer<ActiveMQDestination> topic(*iter);
+        CPPUNIT_ASSERT(topic->isTopic());
+        CPPUNIT_ASSERT(topic->isTemporary() == false);
+        CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    }
+
+    Pointer<cms::Destination> cmsDest = target.dynamicCast<cms::Destination>();
+
+    std::vector<ActiveMQDestination*> topics2 = AdvisorySupport::getAllDestinationAdvisoryTopics(cmsDest.get());
+    CPPUNIT_ASSERT(topics2.size() > 0);
+
+    std::vector<ActiveMQDestination*>::iterator iter2 = topics2.begin();
+    for (; iter2 != topics2.end(); ++iter2) {
+        Pointer<ActiveMQDestination> topic(*iter2);
+        CPPUNIT_ASSERT(topic->isTopic());
+        CPPUNIT_ASSERT(topic->isTemporary() == false);
+        CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    }
+
+    CPPUNIT_ASSERT_EQUAL(topics.size(), topics2.size());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetConnectionAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getConnectionAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".Connection") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetQueueAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getQueueAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".Queue") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetTopicAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getTopicAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".Topic") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetTempQueueAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getTempQueueAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".TempQueue") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetTempTopicAdvisoryTopic() {
+
+    Pointer<ActiveMQDestination> topic(AdvisorySupport::getTempTopicAdvisoryTopic());
+    CPPUNIT_ASSERT(topic != NULL);
+    CPPUNIT_ASSERT(topic->isComposite());
+    CPPUNIT_ASSERT(topic->isTopic());
+    CPPUNIT_ASSERT(topic->isTemporary() == false);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(AdvisorySupport::ADVISORY_TOPIC_PREFIX) == 0);
+    CPPUNIT_ASSERT(topic->getPhysicalName().find(".TempTopic") != std::string::npos);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetConsumerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetProducerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetExpiredMessageTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetExpiredTopicMessageAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetExpiredQueueMessageAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetNoConsumersAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetNoTopicConsumersAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetNoQueueConsumersAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetSlowConsumerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetFastProducerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetMessageDiscardedAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetMessageDeliveredAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetMessageConsumedAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetMessageDLQdAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetMasterBrokerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetNetworkBridgeAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetFullAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testGetDestinationAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsDestinationAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsTempDestinationAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsConnectionAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsProducerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsConsumerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsSlowConsumerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsFastProducerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsMessageConsumedAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsMasterBrokerAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsMessageDeliveredAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsMessageDiscardedAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsMessageDLQdAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsFullAdvisoryTopic() {
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void AdvisorySupportTest::testIsNetworkBridgeAdvisoryTopic() {
+
+}
