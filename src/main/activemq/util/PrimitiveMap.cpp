d8e7cdd3bf9a4ad564a56d1edc49cbadd1b6bb59
Timothy A. Bish <tabish@apache.org>
2012-05-15 15:38:56 +0000

    Add accessor to the PrimitiveMap to get the underlying type for a value stored at the given key.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@1338758 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/activemq/util/PrimitiveMap.cpp
+++ src/main/activemq/util/PrimitiveMap.cpp
@@ -28,7 +28,7 @@ using namespace decaf::util;
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
-PrimitiveMap::PrimitiveMap() : converter() {
+PrimitiveMap::PrimitiveMap() : decaf::util::StlMap<std::string, PrimitiveValueNode>(), converter() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -36,13 +36,13 @@ PrimitiveMap::~PrimitiveMap() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-PrimitiveMap::PrimitiveMap( const decaf::util::Map<std::string, PrimitiveValueNode>& src )
-  : decaf::util::StlMap<std::string, PrimitiveValueNode>( src ), converter() {
+PrimitiveMap::PrimitiveMap(const decaf::util::Map<std::string, PrimitiveValueNode>& src) :
+    decaf::util::StlMap<std::string, PrimitiveValueNode>(src), converter() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-PrimitiveMap::PrimitiveMap( const PrimitiveMap& src )
-  : decaf::util::StlMap<std::string, PrimitiveValueNode>( src ), converter() {
+PrimitiveMap::PrimitiveMap(const PrimitiveMap& src) :
+    decaf::util::StlMap<std::string, PrimitiveValueNode>(src), converter() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -53,9 +53,8 @@ std::string PrimitiveMap::toString() const {
 
     stream << "Begin Class PrimitiveMap:" << std::endl;
 
-    for( std::size_t i = 0; i < keys.size(); ++i ) {
-        stream << "map[" << keys[i] << "] = "
-               << this->get( keys[i] ).toString() << std::endl;
+    for (std::size_t i = 0; i < keys.size(); ++i) {
+        stream << "map[" << keys[i] << "] = " << this->get(keys[i]).toString() << std::endl;
     }
 
     stream << "End Class PrimitiveMap:" << std::endl;
@@ -64,153 +63,137 @@ std::string PrimitiveMap::toString() const {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-bool PrimitiveMap::getBool( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<bool>( node );
+PrimitiveValueNode::PrimitiveType PrimitiveMap::getValueType(const std::string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return node.getType();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setBool( const string& key, bool value ) {
+bool PrimitiveMap::getBool(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<bool> (node);
+}
 
+////////////////////////////////////////////////////////////////////////////////
+void PrimitiveMap::setBool(const string& key, bool value) {
     PrimitiveValueNode node;
-    node.setBool( value );
-
-    this->put( key, node );
+    node.setBool(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-unsigned char PrimitiveMap::getByte( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<unsigned char>( node );
+unsigned char PrimitiveMap::getByte(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<unsigned char> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setByte( const string& key, unsigned char value ) {
+void PrimitiveMap::setByte(const string& key, unsigned char value) {
     PrimitiveValueNode node;
-    node.setByte( value );
-
-    this->put( key, node );
+    node.setByte(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-char PrimitiveMap::getChar( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<char>( node );
+char PrimitiveMap::getChar(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<char> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setChar( const string& key, char value ) {
+void PrimitiveMap::setChar(const string& key, char value) {
     PrimitiveValueNode node;
-    node.setChar( value );
-
-    this->put( key, node );
+    node.setChar(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-short PrimitiveMap::getShort( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<short>( node );
+short PrimitiveMap::getShort(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<short> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setShort( const string& key, short value ) {
+void PrimitiveMap::setShort(const string& key, short value) {
     PrimitiveValueNode node;
-    node.setShort( value );
-
-    this->put( key, node );
+    node.setShort(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-int PrimitiveMap::getInt( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<int>( node );
+int PrimitiveMap::getInt(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<int> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setInt( const string& key, int value ) {
+void PrimitiveMap::setInt(const string& key, int value) {
     PrimitiveValueNode node;
-    node.setInt( value );
-
-    this->put( key, node );
+    node.setInt(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-long long PrimitiveMap::getLong( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<long long>( node );
+long long PrimitiveMap::getLong(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<long long> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setLong( const string& key, long long value ) {
+void PrimitiveMap::setLong(const string& key, long long value) {
     PrimitiveValueNode node;
-    node.setLong( value );
-
-    this->put( key, node );
+    node.setLong(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-double PrimitiveMap::getDouble( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<double>( node );
+double PrimitiveMap::getDouble(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<double> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setDouble( const string& key, double value ) {
+void PrimitiveMap::setDouble(const string& key, double value) {
     PrimitiveValueNode node;
-    node.setDouble( value );
-
-    this->put( key, node );
+    node.setDouble(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-float PrimitiveMap::getFloat( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<float>( node );
+float PrimitiveMap::getFloat(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<float> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setFloat( const string& key, float value ) {
+void PrimitiveMap::setFloat(const string& key, float value) {
     PrimitiveValueNode node;
-    node.setFloat( value );
-
-    this->put( key, node );
+    node.setFloat(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-string PrimitiveMap::getString( const string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert<std::string>( node );
+string PrimitiveMap::getString(const string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<std::string> (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setString( const string& key, const string& value ) {
+void PrimitiveMap::setString(const string& key, const string& value) {
     PrimitiveValueNode node;
-    node.setString( value );
-
-    this->put( key, node );
+    node.setString(value);
+    this->put(key, node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-std::vector<unsigned char> PrimitiveMap::getByteArray( const std::string& key ) const {
-
-    PrimitiveValueNode node = this->get( key );
-    return converter.convert< std::vector<unsigned char> >( node );
+std::vector<unsigned char> PrimitiveMap::getByteArray(const std::string& key) const {
+    PrimitiveValueNode node = this->get(key);
+    return converter.convert<std::vector<unsigned char> > (node);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void PrimitiveMap::setByteArray( const std::string& key, const std::vector<unsigned char>& value ) {
-
+void PrimitiveMap::setByteArray(const std::string& key, const std::vector<unsigned char>& value) {
     PrimitiveValueNode node;
-    node.setByteArray( value );
-
-    this->put( key, node );
+    node.setByteArray(value);
+    this->put(key, node);
 }
