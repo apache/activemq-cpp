20580098a66f4490908dfece4ec6fca54bbca655
Timothy A. Bish <tabish@apache.org>
2012-10-02 22:41:41 +0000

    fix for: https://issues.apache.org/jira/browse/AMQCPP-429
    git-svn-id: https://svn.apache.org/repos/asf/activemq/activemq-cpp/trunk/activemq-cpp@1393223 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/activemq/transport/correlator/ResponseCorrelator.h
+++ src/main/activemq/transport/correlator/ResponseCorrelator.h
@@ -20,14 +20,11 @@
 
 #include <activemq/util/Config.h>
 #include <activemq/transport/TransportFilter.h>
-#include <activemq/transport/correlator/FutureResponse.h>
 #include <activemq/commands/Command.h>
 #include <activemq/commands/Response.h>
-#include <decaf/util/concurrent/Mutex.h>
-#include <decaf/util/concurrent/Concurrent.h>
-#include <decaf/util/concurrent/atomic/AtomicInteger.h>
-#include <map>
-#include <stdio.h>
+
+#include <decaf/lang/Exception.h>
+#include <decaf/lang/Pointer.h>
 
 namespace activemq{
 namespace transport{
@@ -37,68 +34,54 @@ namespace correlator{
     using activemq::commands::Command;
     using activemq::commands::Response;
 
+    class CorrelatorData;
+
     /**
-     * This type of transport filter is responsible for correlating
-     * asynchronous responses with requests.  Non-response messages
-     * are simply sent directly to the CommandListener.  It owns
-     * the transport that it
+     * This type of transport filter is responsible for correlating asynchronous responses
+     * with requests.  Non-response messages are simply sent directly to the CommandListener.
+     * It owns the transport that it
      */
     class AMQCPP_API ResponseCorrelator : public TransportFilter {
     private:
 
-        /**
-         * The next command id for sent commands.
-         */
-        decaf::util::concurrent::atomic::AtomicInteger nextCommandId;
-
-        /**
-         * Map of request ids to future response objects.
-         */
-        std::map<unsigned int, Pointer<FutureResponse> > requestMap;
-
-        /**
-         * Sync object for accessing the request map.
-         */
-        decaf::util::concurrent::Mutex mapMutex;
-
-        /**
-         * Flag to indicate the closed state.
-         */
-        bool closed;
+        CorrelatorData* impl;
 
     public:
 
         /**
-         * Constructor.
-         * @param next the next transport in the chain
+         * Creates a new ResponseCorrelator transport filter that wraps the given transport.
+         *
+         * @param next
+         *      the next transport in the chain
+         *
+         * @throws NullPointerException if next if NULL.
          */
-        ResponseCorrelator( const Pointer<Transport>& next );
+        ResponseCorrelator(const Pointer<Transport>& next);
 
         virtual ~ResponseCorrelator();
 
     public:  // Transport Methods
 
-        virtual void oneway( const Pointer<Command>& command );
+        virtual void oneway(const Pointer<Command>& command);
 
-        virtual Pointer<Response> request( const Pointer<Command>& command );
+        virtual Pointer<Response> request(const Pointer<Command>& command);
 
-        virtual Pointer<Response> request( const Pointer<Command>& command, unsigned int timeout );
+        virtual Pointer<Response> request(const Pointer<Command>& command, unsigned int timeout);
 
         virtual void start();
 
         virtual void close();
 
         /**
-         * This is called in the context of the nested transport's
-         * reading thread.  In the case of a response object,
-         * updates the request map and notifies those waiting on the
-         * response.  Non-response messages are just delegated to
-         * the command listener.
+         * This is called in the context of the nested transport's reading thread.  In
+         * the case of a response object, updates the request map and notifies those
+         * waiting on the response.  Non-response messages are just delegated to the
+         * command listener.
          *
          * @param command
          *      The received from the nested transport.
          */
-        virtual void onCommand( const Pointer<Command>& command );
+        virtual void onCommand(const Pointer<Command>& command);
 
         /**
          * Event handler for an exception from a command transport.
@@ -108,7 +91,11 @@ namespace correlator{
          * @param ex
          *      The exception that was caught.
          */
-        virtual void onTransportException( Transport* source, const decaf::lang::Exception& ex );
+        virtual void onException(const decaf::lang::Exception& ex);
+
+    private:
+
+        void dispose(const Pointer<decaf::lang::Exception> ex);
 
     };
 
